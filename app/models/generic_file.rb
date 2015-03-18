311b574865fbe74cc032bf975430d13d336d6d8f
Hui Zhang <hui.zhang@oregonstate.edu>
2015-03-18 15:01:10 -0700

    change metadata fields

--- app/models/generic_file.rb
+++ app/models/generic_file.rb
@@ -6,7 +6,79 @@ class GenericFile < ActiveFedora::Base
     index.as :stored_searchable, :facetable
   end
 
-  property :doi, predicate: ::RDF::Vocab::Identifiers.doi do |index|
+  property :contributor, predicate: ::RDF::DC.contributor do |index|
     index.as :stored_searchable, :facetable
+  end
+  property :temporal, predicate: ::RDF::DC.temporal do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :creator, predicate: ::RDF::DC.creator do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :abstract, predicate: ::RDF::DC.abstract do |index|
+    index.as :stored_searchable
+  end
+  property :tableOfContents, predicate: ::RDF::DC.tableOfContents do |index|
+    index.as :stored_searchable
+  end
+  property :description, predicate: ::RDF::DC.description do |index|
+    index.as :stored_searchable
+  end
+  property :format, predicate: ::RDF::DC.format do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :bibliographicCitation, predicate: ::RDF::DC.bibliographicCitation do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :language, predicate: ::RDF::DC.language do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :publisher, predicate: ::RDF::DC.publisher do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :provenance, predicate: ::RDF::DC.provenance do |index|
+    index.as :stored_searchable
+  end
+  property :isReferncedBy, predicate: ::RDF::DC.isReferencedBy do |index|
+    index.as :stored_searchable
+  end
+  property :relation, predicate: ::RDF::DC.relation do |index|
+    index.as :stored_searchable
+  end
+  property :rights, predicate: ::RDF::DC.rights do |index|
+    index.as :stored_searchable
+  end
+  property :subject, predicate: ::RDF::DC.subject do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :title, predicate: ::RDF::DC.title do |index|
+    index.as :stored_searchable
+  end
+  property :type, predicate: ::RDF::DC.type do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :date, predicate: ::RDF::DC.date do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :isCitedBy, predicate: ::RDF::DC.isCitedBy do |index|
+    index.as :stored_searchable
+  end
+  property :isIdenticalTo, predicate: ::RDF::DC.isIdenticalTo do |index|
+    index.as :stored_searchable
+  end
+  property :isPartOf, predicate: ::RDF::DC.isPartOf do |index|
+    index.as :stored_searchable
+  end
+  property :isVersionOf, predicate: ::RDF::DC.isVersionOf do |index|
+    index.as :stored_searchable
+  end
+  property :doi, predicate: ::RDF::Vocab::Identifiers.doi do |index|
+    index.as :stored_searchable
+  end
+  property :uri, predicate: ::RDF::Vocab::Identifiers.uri do |index|
+    index.as :stored_searchable
+  end
+  property :hdl, predicate: ::RDF::Vocab::Identifiers.hdl do |index|
+    index.as :stored_searchable
   end
 end
