4f5180358aaae2b509204e2766b1043db9bf0839
Trey Terrell <trey.terrell@oregonstate.edu>
2015-05-07 15:58:03 -0700

    Fix spec to point to Devise controller.

--- spec/routing/destroy_user_session_spec.rb
+++ spec/routing/destroy_user_session_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "destroy user session" do
   context "when signed in as a user" do
     it "routes /users/sign_out to devise/sessions#destroy" do
-      expect(get("/users/sign_out")).to route_to("sessions#destroy")
+      expect(get("/users/sign_out")).to route_to("devise/sessions#destroy")
     end
   end
 end
