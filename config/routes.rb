3d918728e76a465d3cb3978db1a970d66a28918b
Trey Terrell <trey.terrell@oregonstate.edu>
2015-05-07 15:54:55 -0700

    Fix spec + routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   blacklight_for :catalog
-  devise_for :users, :controllers => {:cas_sessions => "sessions"}
+  devise_for :users
   Hydra::BatchEdit.add_routes(self)
   # This must be the very last route in the file because it has a catch-all route for 404 errors.
     # This behavior seems to show up only in production mode.
