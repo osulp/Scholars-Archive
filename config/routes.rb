de33d033ced68888dc95501022154aeae868c675
luisgreg99 <luisram90_101@hotmail.com>
2015-05-11 15:10:21 -0700

    updated routes to mount sufia at the end as suggested in the comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
   blacklight_for :catalog
   devise_for :users
   Hydra::BatchEdit.add_routes(self)
-  # This must be the very last route in the file because it has a catch-all route for 404 errors.
-    # This behavior seems to show up only in production mode.
-    mount Sufia::Engine => '/'
-  root to: 'homepage#index'
-  
+
   resource :help, :controller => :help, :only => [] do
     get "faculty"
     get "graduate"
@@ -17,4 +13,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/users/sign_out", :to => "sessions#destroy"
   end
+
+  # This must be the very last route in the file because it has a catch-all route for 404 errors.
+  # This behavior seems to show up only in production mode.
+  root to: 'homepage#index'
+
+  mount Sufia::Engine => '/'
 end
