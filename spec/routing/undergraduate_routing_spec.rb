6c9818443924adaaaeaec04c5fb16d7250d34efd
luisgreg99 <luisram90_101@hotmail.com>
2015-05-11 09:25:23 -0700

    updated help routing spec to be more DRY

--- spec/routing/undergraduate_routing_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "help routing" do
-  it "should route /help/undergraduate to help#undergraduate" do
-    expect(get "/help/undergraduate").to route_to "help#undergraduate"
-  end
-
-end
