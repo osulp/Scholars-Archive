a42da09954b74b88fc32ed5fe8037f26fc62189b
luisgreg99 <luisram90_101@hotmail.com>
2015-05-12 11:44:15 -0700

    updated devise to authenticate on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   # Connects this user object to Hydra behaviors.
   include Hydra::User# Connects this user object to Sufia behaviors. 
- include Sufia::User
+  include Sufia::User
   include Sufia::UserUsageStats
 
 
 
   attr_accessible :email, :password, :password_confirmation if Rails::VERSION::MAJOR < 4
-# Connects this user object to Blacklights Bookmarks. 
+  # Connects this user object to Blacklights Bookmarks. 
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
