c88e0d3ab224ce9f50b604fc8bb748a00de916f3
Brandon Straley <straleyb@STRALEYB-MBP15.tss.oregonstate.edu>
2015-07-20 10:10:29 -0700

    Removed unnecessary label finding code

--- app/models/triple_powered_resource.rb
+++ app/models/triple_powered_resource.rb
@@ -3,19 +3,4 @@ class TriplePoweredResource < ActiveTriples::Resource
   def repository
     @repository ||= MarmottaRepository.new(rdf_subject)
   end
-
-  def preferred_label
-    @preferred_label ||= composite_label.to_s
-  end
-
-  private
-
-  def composite_label
-    CompositeLabel.new(label_finder.labels)
-  end
-
-  def label_finder
-    LanguageSelector::LabelFinderFactory.new(AllLabelFinder).new(self)
-  end
-
 end
