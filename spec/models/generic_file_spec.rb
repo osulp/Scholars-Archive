5a5ab38bc0bcde30d40e971e8c6b1d96fbf2e99d
Hui Zhang <hui.zhang@oregonstate.edu>
2015-07-21 12:01:53 -0700

    #128 add enriched solr model to spec

--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -1,35 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe GenericFile do
-  subject { described_class.new(property) }
-  let(:property) { SolrProperty.new("subject_ssim", value) }
-  let(:value) { "http://localhost:40/1" }
-  describe "#properties" do
-    context "when there's subjects" do
-      it "should return a nice property" do
-        build_resource(uri: value, label: "LC Subject").tap do |r|
-          r << [r.rdf_subject, RDF::SKOS.altLabel, "Alternative"]
-          r << [r.rdf_subject, RDF::SKOS.altLabel, "More labels"]
-        end.persist!
-
-        property = subject.properties.first
-        expect(property.values).to eq ["Alternative", "More labels"]
-        expect(property.key).to eq "subject_alternative_label"
-        expect(property.solr_identifier).to eq "ssim"
-      end
-    end
-    context "when there's no label" do
-      it "should return an empty array property" do
-        expect(subject.properties.first.values).to eq []
-      end
-    end
+RSpec.describe GenericFile do  
+  let(:file) do
+    GenericFile.new(id)
+  end
+  let(:id) { 'someid' }
+  subject{ file }
+  before do
+    subject.subject = [uri]
+  end
+  let(:uri) { resource.rdf_subject }
+  let(:resource) do
+    r = TriplePoweredResource.new("http://localhost:40/1")
+    r.preflabel = "Test"
+    r.persist!
+    r
   end
 
-  it "should be able to persist" do
-    expect{GenericFile.new.save}.not_to raise_error
-  end
-
-  def build_resource(properties = {})
-    return TriplePoweredResource.new(properties)
+  describe "#atomic update" do
+    let(:connection) { ActiveFedora.solr.conn }
+    let(:solr_document) do
+      s = ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(id)}").first
+    end
+    let(:enriched_solr_document) do
+      es = EnrichedSolrDocument.new(solr_document).to_solr
+    end
+    let(:solr_json) do
+      sj = enriched_solr_document.to_json
+    end
+    before do
+      connection.update(
+        :params => { softCommit: true },
+        :data => solr_json,
+        :headers => { 'Content-Type' => 'application/json' }
+      )
+    end
+      
+    it "should return enriched solr field" do
+      expect(ActiveFedora::SolrService.query("id:#{id}").first["lcsubject_preferred_label_ssim"]).to eq ["Test"]
+    end
   end
 end
