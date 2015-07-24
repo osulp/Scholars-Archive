0f5f88848f6ce1aa40f04b2317ed34877ba4353e
Hui Zhang <hui.zhang@oregonstate.edu>
2015-07-24 11:07:40 -0700

    add spec for enricher genericfile

--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -1,40 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe GenericFile do  
-  let(:file) do
-    GenericFile.new(id) { |file| file.apply_depositor_metadata('zhanghu') }
-  end
-  let(:id) do
-    id = 'id001'
-  end  
-
-  before do
-    file.save!
-    file.subject = [uri]
-  end
-  
-  let(:uri) { resource.rdf_subject }
-  let(:resource) do
-    r = TriplePoweredResource.new("http://localhost:40/1")
-    r.preflabel = "Test"
-    r.persist!
-    r
-  end
-
-  describe "#atomic update" do
-    before do
-      file.save!
-    end   
-
-    it "should return enriched solr field" do
-      enricher = Enricher.new(id)
-      enricher.enrich!
-      expect(ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(id)}").first["subject_preferred_label_ssim"]).to eq ["Test"]
-    end
-  end
-
+RSpec.describe GenericFile do
   it "should be able to persist" do
     expect{GenericFile.new.save}.not_to raise_error
   end
-
 end
