c5e4160de153c3e9b8ca11437862d40a8c3147da
Hui Zhang <hui.zhang@oregonstate.edu>
2015-07-23 18:16:57 -0700

    #128: use enricher

--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -2,52 +2,34 @@ require 'rails_helper'
 
 RSpec.describe GenericFile do  
   let(:file) do
-    #GenericFile.new(id: 'oneid_2') { |file| file.apply_depositor_metadata('terrellt') }
-    GenericFile.new(id) { |file| file.apply_depositor_metadata('terrellt') }
+    GenericFile.new(id) { |file| file.apply_depositor_metadata('zhanghu') }
   end
   let(:id) do
-    id = 'id006'
+    id = 'id001'
   end  
 
   before do
     file.save!
-    subject { file }
-    subject.subject = [uri]
-    subject.save!
+    file.subject = [uri]
   end
   
   let(:uri) { resource.rdf_subject }
   let(:resource) do
     r = TriplePoweredResource.new("http://localhost:40/1")
-    r.preflabel = "Test2"
+    r.preflabel = "Test"
     r.persist!
     r
   end
 
-  let(:connection) { ActiveFedora.solr.conn }
-  let(:solr_document) do
-    ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(id)}").first
-  end
-  let(:enriched_solr_document) do
-    EnrichedSolrDocument.new(solr_document).to_solr
-  end
-  let(:solr_json) do
-    # array is required for to_json 
-    [enriched_solr_document].to_json
-  end
-  
   describe "#atomic update" do
     before do
-    file.save!
-    binding.pry
-      connection.update(
-        :params => { softCommit: true },
-        :data => solr_json,
-        :headers => { 'Content-Type' => 'application/json' }
-      )
-    end
-      
+      file.save!
+    end   
+
     it "should return enriched solr field" do
+      enricher = Enricher.new(id)
+      binding.pry
+      enricher.enrich!
       expect(ActiveFedora::SolrService.query("id:#{RSolr.solr_escape(id)}").first["subject_preferred_label_ssim"]).to eq ["Test"]
     end
   end
