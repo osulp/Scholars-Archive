99d24add5e05cdf9ff03cf91fd24c9797bfb07df
Brandon Straley <straleyb@10-249-106-134.wireless.oregonstate.edu>
2015-07-24 11:27:24 -0700

    using setting for marmotta instead of hardcoded url

--- lib/scholars_archive.rb
+++ lib/scholars_archive.rb
@@ -1,11 +1,8 @@
 module ScholarsArchive
   def marmotta
-    @marmotta ||= Marmotta::Connection.new(uri: marmotta_url, context: Rails.env)
+    @marmotta ||= Marmotta::Connection.new(uri: Settings.marmotta.url, context: Rails.env)
   end
 
-  def marmotta_url
-    "http://localhost:8983/marmotta"
-  end
   module_function :marmotta
   module_function :marmotta_url
 end
