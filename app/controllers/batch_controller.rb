7c9659df771c61615dbc970f0d3e390ab4c6b0f5
luisgreg99 <luisram90_101@hotmail.com>
2015-08-18 16:15:08 -0700

    added default publisher and language for edit form

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -3,4 +3,12 @@ class BatchController < ApplicationController
   include Sufia::BatchControllerBehavior
 
   self.edit_form_class = BatchEditForm
+
+  def edit
+    @batch = Batch.find_or_create(params[:id])
+    @form = edit_form
+    @form["publisher"] = [t('sufia.default_publisher')] if @form["publisher"].first.empty?
+    @form["language"] = [RDF::URI(t('sufia.default_language_uri'))] if @form["language"].first.empty?
+  end
+
 end
