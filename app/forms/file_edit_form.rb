da05335ac6d8268e1d569c4c822d2564e0ddf0b3
Trey Terrell <trey.terrell@oregonstate.edu>
2015-08-27 08:52:24 -0700

    Move around delegations.

--- app/forms/file_edit_form.rb
+++ app/forms/file_edit_form.rb
@@ -1,14 +1,11 @@
 # app/forms/file_edit_form.rb
 class FileEditForm < FilePresenter
   include HydraEditor::Form
-  delegate :validators, :to => :model
+  delegate :validators, :content, :to => :model
+  delegate :has_content?, :to => :content
   include HydraEditor::Form::Permissions
   self.model_class = GenericFile
   self.required_fields = [:title, :creator, :rights, :keyword]
-
-  def has_content?
-    model.content.has_content?
-  end
 
   def initialize_fields
     model.nested_authors.build
