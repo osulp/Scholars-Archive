5faf248e6b6e8c31bccbe839670e74147afd5618
luisgreg99 <luisram90_101@hotmail.com>
2015-10-14 10:42:02 -0700

    updated capybara settings for poltergeist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,10 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'capybara/rspec'
-require 'capybara/poltergeist'
-require 'capybara/rails'
-Capybara.javascript_driver = :poltergeist
 
 include Warden::Test::Helpers
 Warden.test_mode!
