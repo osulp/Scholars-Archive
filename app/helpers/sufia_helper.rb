1be6b62d20caa54a609112e84f5620fe61a335d3
luisgreg99 <luisram90_101@hotmail.com>
2015-12-14 12:15:01 -0800

    updated sufia helper to properly render uri labels

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -2,4 +2,13 @@ module SufiaHelper
   include ::BlacklightHelper
   include Sufia::BlacklightOverride
   include Sufia::SufiaHelperBehavior
+
+  def link_to_facet(field, field_string)
+    if field['preflabel']
+      link_to(field['preflabel'].first, catalog_index_path(add_facet_params(field_string, field['id'])))
+    else
+      link_to(field, add_facet_params(field_string, field).merge!(controller: "catalog", action: "index"))
+    end
+  end
+
 end
