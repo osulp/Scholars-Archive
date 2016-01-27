866ba004746a75676164a5e35752aeaf86c60919
luisgreg99 <luisram90_101@hotmail.com>
2016-01-27 14:46:07 -0800

    removed unnecessary lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,7 @@ class ApplicationController < ActionController::Base
   include Hydra::Controller::ControllerBehavior
   layout 'sufia-one-column'
 
-  # after_filter do
-  #   if response.headers['Content-Length'].present?
-  #     response.headers['Content-Length'] = response.headers['Content-Length'].to_s
-  #   end
-  # end
-  #
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
 end
