987cfcb872dad6155d448e5e98e553eefdd39df8
deploy-rails <deploy-rails@lib-saapp1.library.oregonstate.edu>
2016-02-22 16:39:57 -0800

    Cap deploy work

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,5 @@
 require_relative 'production'
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'hydra-sa-dev.library.oregonstate.edu' }
+  config.action_mailer.default_url_options = { host: 'lib-saapp1.library.oregonstate.edu' }
 end
