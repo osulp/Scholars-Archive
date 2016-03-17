d8db52b20fea03cbf43211f206117e687bec661b
Josh Gum <josh.gum@oregonstate.edu>
2016-03-17 09:54:31 -0700

    adds UserMailer and notifications related to sharing archive with users

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -3,6 +3,8 @@ class GenericFilesController < ApplicationController
   include Sufia::Controller
   include ScholarsArchive::FilesControllerBehavior
 
+  after_filter :notify_if_shared, :only => :update
+
   self.presenter_class = FilePresenter
   self.edit_form_class = FileEditForm
 
@@ -34,8 +36,32 @@ class GenericFilesController < ApplicationController
 
   private
 
-  def resource_file_path 
+  def resource_file_path
     Sufia::Engine.routes.url_helpers.generic_file_url(stats.id, :only_path => false, :host => request.host)
+  end
+
+  # after a file has been updated, determine if there are users to notify that
+  # they have access shared to them
+  def notify_if_shared
+
+    if !flash[:error]
+      unless params[:generic_file].nil?
+        # updates to description or version will not include the permission
+        # attributes, so no notification is relevant
+        users = params[:generic_file][:permissions_attributes] || {}
+        users.each_pair do |k,u|
+          binding.pry
+          if u[:type] == "user"
+            user = User.find_by_username(u[:name])
+            if user.email.empty?
+              UserMailer.support_invalid_user(user).deliver_now
+            else
+              UserMailer.shared_access_to(user, @generic_file, u[:access]).deliver_now
+            end
+          end
+        end
+      end
+    end
   end
 
 
