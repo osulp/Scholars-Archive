2dc2c9deb4e2bd543858c0187f9f1b9fa8cc9227
Josh Gum <josh.gum@oregonstate.edu>
2016-03-18 13:16:49 -0700

    handles CAS authenticated users missing email address, notification, and redirecting user to update thier profile

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
 class UserMailer < ActionMailer::Base
   default :from => 'noreply@oregonstate.edu'
 
-  def support_invalid_user(user)
+  def support_invalid_user(user, message)
     @user = user
+    @message = message ||= ""
     mail(:from => APPLICATION_CONFIG['notifications']['support_email'], :to => APPLICATION_CONFIG['notifications']['support_email'], :subject => '[ScholarsArchive] Invalid User Detected')
   end
   def shared_access_to(user, generic_file, access_level)
     @user = user
     @generic_file = generic_file
     @access_level = access_level
-    binding.pry
     mail(:from => APPLICATION_CONFIG['notifications']['support_email'], :to => @user.email, :subject => "[ScholarsArchive] You've been granted #{@access_level} access to '#{@generic_file[:title].first}'")
   end
 end
