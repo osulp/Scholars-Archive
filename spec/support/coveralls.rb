7b9e3d762d834a3113ea989f01169b1c48cabf29
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-03-21 13:53:03 -0700

    Coveralls now working properly. Load before all else.

--- spec/support/coveralls.rb
+++
@@ -1,8 +0,0 @@
-require 'simplecov'
-require 'coveralls'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-    SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
-])
-SimpleCov.start 'rails'
