a8a1c6806837ab18c4e3cd8f9330c8355b2a4224
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-03-21 11:01:47 -0700

    Properly fix coveralls formatter

--- spec/support/coveralls.rb
+++ spec/support/coveralls.rb
@@ -1,8 +1,8 @@
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter::new(
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
       Coveralls::SimpleCov::Formatter
-)
+])
 SimpleCov.start 'rails'
