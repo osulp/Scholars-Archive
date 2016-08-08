1d7b3f10b00f8c08b561dc86802e20a40b7e92c9
luisgreg99 <luisram90_101@hotmail.com>
2016-08-08 10:44:43 -0700

    updated rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,35 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter])
+SimpleCov.start 'rails'
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'webmock/rspec'
+require 'capybara/rspec'
+require 'triplestore_adapter'
+
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, {js_errors: false, timeout: 60,debug: true, phantomjs_logger: true, stdout: true, phantomjs_options: ['--ignore-ssl-errors=yes','--load-images=yes']})
+end
+
 include Warden::Test::Helpers
 Warden.test_mode!
+WebMock.allow_net_connect!
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -37,6 +60,10 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.before(:suite) do
+    Capybara.current_driver = Capybara.javascript_driver
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -56,10 +83,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  if Devise::VERSION >= '4.2'
-    # This is for an unreleased version of Devise (will either be 4.2 or 5.0)
-    config.include Devise::Test::ControllerHelpers, type: :controller
-  else
-    config.include Devise::TestHelpers, type: :controller
-  end
 end
