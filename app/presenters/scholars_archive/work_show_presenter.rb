03f2671a22b3a25d0f3fbae9f596d26c1911b9c3
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-08-09 15:37:46 -0700

    Got presenters working

--- app/presenters/scholars_archive/work_show_presenter.rb
+++ app/presenters/scholars_archive/work_show_presenter.rb
@@ -1,7 +1,7 @@
 module ScholarsArchive
-  class WorkShowPresenter < ::Sufia::WorkShowPresenter
+  class WorkShowPresenter < Sufia::WorkShowPresenter
     # delegate fields from Sufia::Works::Metadata to solr_document
-    delegate :based_near, :related_url, :depositor, :identifier, :resource_type, :accepted, :available, :copyrighted, :collected, :issued, :valid, :itemtype, :nested_geo_points, :nested_geo_bbox, :nested_geo_location, to: :solr_document
+    delegate :accepted, :available, :copyrighted, :collected, :issued, :valid, :nested_geo_points, :nested_geo_bbox, :nested_geo_location, to: :solr_document
 
   end
 end
