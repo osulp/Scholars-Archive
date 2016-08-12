57a1bcb70156d5879bb387ce6847407847e32733
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-08-12 09:29:29 -0700

    Remove duplicate file and old behavior

--- app/controllers/curation_concerns/generic_works_controller.rb
+++ app/controllers/curation_concerns/generic_works_controller.rb
@@ -7,7 +7,6 @@ module CurationConcerns
     # Adds Sufia behaviors to the controller.
     include Sufia::WorksControllerBehavior
     include ScholarsArchive::StatsControllerBehavior
-    include ScholarsArchive::WorksControllerBehavior
     before_filter :update_nested_geo_location_attributes, :only => [:update, :new]
 
     self.curation_concern_type = GenericWork
