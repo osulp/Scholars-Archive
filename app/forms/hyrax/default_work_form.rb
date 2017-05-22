d4bfd7261ee485ff94decba5175c55d6a7aa45fb
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-05-22 10:18:57 -0700

    Adds all terms to both ETD and Default forms

--- app/forms/hyrax/default_work_form.rb
+++ app/forms/hyrax/default_work_form.rb
@@ -4,14 +4,18 @@ module Hyrax
   class DefaultWorkForm < Hyrax::Forms::WorkForm
     include ::ScholarsArchive::TriplePoweredProperties::TriplePoweredForm
     self.model_class = ::DefaultWork
-    self.terms += [:resource_type, :date_available, :date_copyright, :date_issued, :date_collected, :date_valid, :date_accepted, :replaces]
+    self.terms += [:doi, :alt_title , :license, :based_near, :resource_type, :date_available, :date_copyright, :date_issued, :date_collected, :date_valid, :date_accepted, :replaces, :hydrologic_unit_code, :funding_body, :funding_statement, :in_series, :tableofcontents, :bibliographic_citation, :peerreviewed, :additional_information, :digitization_spec, :file_extent, :file_format, :dspace_community, :dspace_collection]
 
     def self.build_permitted_params
       super + self.date_terms
     end
 
+    def primary_terms
+      super + [:doi, :based_near, :alt_title, :license, :resource_type, :abstract]
+    end
+
     def secondary_terms
-      super - self.date_terms
+      super - self.date_terms - [:license, :resource_type, :description] + [:hydrologic_unit_code, :funding_body, :funding_statement, :in_series, :tableofcontents, :bibliographic_citation, :peerreviewed, :additional_information, :digitization_spec, :file_extent, :file_format, :dspace_community, :dspace_collection]
     end
 
     def self.date_terms
--- app/forms/hyrax/etd_form.rb
+++ app/forms/hyrax/etd_form.rb
@@ -1,12 +1,24 @@
 # Generated via
 #  `rails generate hyrax:work Etd`
 module Hyrax
-  class EtdForm < Hyrax::Forms::WorkForm
+  class EtdForm < Hyrax::DefaultWorkForm
     self.model_class = ::Etd
-    self.terms += [:resource_type, :replaces]
+    self.terms += [:degree_level, :degree_name, :degree_field, :contributor_advisor, :contributor_committeemember, :graduation_year, :degree_discipline]
 
     def date_terms
       []
+    end
+
+    def primary_terms
+      super + [:degree_level, :degree_name, :degree_field, :contributor_advisor, :contributor_committeemember, :graduation_year]
+    end
+
+    def secondary_terms
+      super - self.date_terms + [:degree_discipline]
+    end
+
+    def date_terms
+      super
     end
   end
 end
