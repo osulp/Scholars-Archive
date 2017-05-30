b5117673e8dd43f836a0ed4ce42e3a8812cb5fda
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-05-30 13:51:41 -0700

    Adds missing info back to etd form. Remove dup form field

--- app/presenters/etd_presenter.rb
+++ app/presenters/etd_presenter.rb
@@ -1,3 +1,3 @@
 class EtdPresenter < DefaultWorkPresenter
-  delegate :contributor_advisor, :contributor_committeemember, :degree_discipline, :degree_level, :degree_name, :degree_field, :degree_grantor, :graduation_year,to: :solr_document
+  delegate :contributor_advisor, :contributor_committeemember, :degree_discipline, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year,to: :solr_document
 end
