e913f39be32b8d10d8bfc701a281c1c9781608e5
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-06-09 15:54:54 -0700

    Adds proper facets and multiplicity.

--- app/models/concerns/scholars_archive/oer_metadata.rb
+++ app/models/concerns/scholars_archive/oer_metadata.rb
@@ -4,23 +4,23 @@ module ScholarsArchive
 
     included do
       property :interactivity_type, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/interactivityType") do |index|
-        index.as :stored_searchable, :facetable
-      end
-
-      property :is_based_on_url, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/isBasedOnUrl") do |index|
         index.as :stored_searchable
       end
 
-      property :learning_resource_type, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/learningResourceType") do |index|
+      property :is_based_on_url, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/isBasedOnUrl"), multiple: false do |index|
         index.as :stored_searchable
       end
 
-      property :time_required, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/timeRequired") do |index|
-        index.as :stored_searchable, :facetable
+      property :learning_resource_type, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/learningResourceType"), multiple: false do |index|
+        index.as :stored_searchable
       end
 
-      property :typical_age_range, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/typicalAgeRange") do |index|
-        index.as :stored_searchable, :facetable
+      property :time_required, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/timeRequired"), multiple: false do |index|
+        index.as :stored_searchable
+      end
+
+      property :typical_age_range, predicate: ::RDF::URI.new("http://purl.org/dcx/lrmi-terms/typicalAgeRange"), multiple: false do |index|
+        index.as :stored_searchable
       end
     end
   end
