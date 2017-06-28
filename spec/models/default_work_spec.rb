92494bb57bdda229433bc139fea7a0dabb9bd3b2
luisgreg99 <luisram90_101@hotmail.com>
2017-06-28 08:26:17 -0700

    added additional spec for dates with invalid format

--- spec/models/default_work_spec.rb
+++ spec/models/default_work_spec.rb
@@ -16,11 +16,24 @@ RSpec.describe DefaultWork do
     let(:date) { "2022-01-01" }
     let(:facet) { asset.to_solr["date_facet_yearly_ssim"] }
 
-    context "when given an invalid date" do
+    context "when given date is nil" do
       let(:asset) do
         g = described_class.new(title: ['test'], keyword: ['test'])
         g.attributes = {
             :date_created => nil
+        }
+        g.save!(:validate => false)
+        g
+      end
+      it "should be blank" do
+        expect(facet).to be_nil
+      end
+    end
+    context "when given an invalid date" do
+      let(:asset) do
+        g = described_class.new(title: ['test'], keyword: ['test'])
+        g.attributes = {
+            :date_created => "typo2011-01-01"
         }
         g.save!(:validate => false)
         g
