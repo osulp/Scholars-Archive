e6c91c3ea0241497282f15bb4bad7389373f4571
Hui Zhang <hui.zhang@oregonstate.edu>
2017-07-14 16:06:01 -0700

    update other files required for the new field

--- app/forms/scholars_archive/oer_work_form_behavior.rb
+++ app/forms/scholars_archive/oer_work_form_behavior.rb
@@ -2,13 +2,13 @@ module ScholarsArchive
   module OerWorkFormBehavior
     extend ActiveSupport::Concern
     included do
-      self.terms += [:resource_type, :is_based_on_url, :interactivity_type, :learning_resource_type, :typical_age_range, :time_required]
+      self.terms += [:resource_type, :is_based_on_url, :interactivity_type, :learning_resource_type, :typical_age_range, :time_required, :duration]
       def primary_terms
         super
       end
 
       def secondary_terms
-        super - self.date_terms + [:is_based_on_url, :interactivity_type, :learning_resource_type, :typical_age_range, :time_required]
+        super - self.date_terms + [:is_based_on_url, :interactivity_type, :learning_resource_type, :typical_age_range, :time_required, :duration]
       end
     end
   end
