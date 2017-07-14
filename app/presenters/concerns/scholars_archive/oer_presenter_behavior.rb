9a3552c825cd6750909c48b1a55c64480a247940
Hui Zhang <hui.zhang@oregonstate.edu>
2017-07-14 11:22:03 -0700

    add duration to OER

--- app/presenters/concerns/scholars_archive/oer_presenter_behavior.rb
+++ app/presenters/concerns/scholars_archive/oer_presenter_behavior.rb
@@ -6,7 +6,8 @@ module ScholarsArchive
                :is_based_on_url,
                :learning_resource_type,
                :time_required,
-               :typical_age_range,  to: :solr_document
+               :typical_age_range,
+               :duration,  to: :solr_document
     end
   end
 end
