cc5749df3d8b009802e84e000bd4cf01c617c1f1
luisgreg99 <luisram90_101@hotmail.com>
2017-07-17 17:05:28 -0700

    added email notification objects for ScholarsArchive
    updated one_step workflow to use SA notifications
    
    updated one_step workflow
    
    updated remaining workflows to use ScholarsArchive notifications
    
    updated actor

---
+++ app/services/scholars_archive/workflow/pending_review_notification.rb
@@ -0,0 +1,19 @@
+module ScholarsArchive
+  module Workflow
+    class PendingReviewNotification < Hyrax::Workflow::AbstractNotification
+      private
+
+      def subject
+        'Deposit needs review'
+      end
+
+      def message
+        "The item: #{title} (#{link_to work_id, document_path}) was deposited by #{user.user_key} and is awaiting review. #{comment}"
+      end
+
+      def users_to_notify
+        super << user
+      end
+    end
+  end
+end
