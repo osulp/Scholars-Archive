4187315148ea48eaf504057d912a3a037171f665
luisgreg99 <luisram90_101@hotmail.com>
2017-07-20 17:09:44 -0700

    set proper recipients in workflow config and email notification objects
    updated workflows and notification objects
    
    updated workflows
    
    updated workflows

--- app/services/scholars_archive/workflow/changes_required_notification.rb
+++ app/services/scholars_archive/workflow/changes_required_notification.rb
@@ -13,7 +13,7 @@ module ScholarsArchive
 
       def users_to_notify
         user_key = document.depositor
-        super << ::User.find_by(email: user_key)
+        [::User.find_by(username: user_key)]
       end
     end
   end
