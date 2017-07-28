6ada917f66f4ab9680f7ba9badc55d1d80d4027e
luisgreg99 <luisram90_101@hotmail.com>
2017-07-28 13:14:35 -0700

    implemented validate function for add_other_field_option actor
    added err counter
    
    added specs for validate method

--- spec/actors/scholars_archive/actors/add_other_field_option_actor_spec.rb
+++ spec/actors/scholars_archive/actors/add_other_field_option_actor_spec.rb
@@ -41,6 +41,48 @@ RSpec.describe ScholarsArchive::Actors::AddOtherFieldOptionActor do
         expect(other_options.second.name).to be_in [test_degree_field_other, test_degree_level_other]
       end
     end
+    context 'with invalid other values selected for degree_field and degree_level' do
+      let(:attributes) { { title: ["test"], creator: ["Blah"], rights_statement: ["blah.blah"], resource_type: ["blah"], degree_field: "Other", degree_level: "Other" } }
+      let(:test_degree_level_other) { "Certificate" }
+      let(:test_degree_field_other) { "Zoology" }
+
+      before do
+        allow(terminator).to receive(:create).with(Hyrax::Actors::Environment).and_return(true)
+        curation_concern.apply_depositor_metadata(user.user_key)
+        curation_concern.degree_field_other = test_degree_field_other
+        curation_concern.degree_level_other = test_degree_level_other
+        curation_concern.save!
+      end
+      it "raises error if the degree level already exists" do
+        expect(subject.create(env)).to be false
+        expect(curation_concern.errors[:degree_level_other].first).to eq 'This degree level already exists, please select from the list above.'
+      end
+      it "raises error if the degree field already exists" do
+        expect(subject.create(env)).to be false
+        expect(curation_concern.errors[:degree_field_other].first).to eq 'This degree field already exists, please select from the list above.'
+      end
+    end
+    context 'with blank "other" values selected for degree_level and degree_field' do
+      let(:attributes) { { title: ["test"], creator: ["Blah"], rights_statement: ["blah.blah"], resource_type: ["blah"], degree_field: "Other", degree_level: "Other" } }
+      let(:test_degree_level_other) { "" }
+      let(:test_degree_field_other) { "" }
+
+      before do
+        allow(terminator).to receive(:create).with(Hyrax::Actors::Environment).and_return(true)
+        curation_concern.apply_depositor_metadata(user.user_key)
+        curation_concern.degree_field_other = test_degree_field_other
+        curation_concern.degree_level_other = test_degree_level_other
+        curation_concern.save!
+      end
+      it "raises error if degree_level is blank" do
+        expect(subject.create(env)).to be false
+        expect(curation_concern.errors[:degree_level_other].first).to eq "Please provide a value for 'Other' degree level."
+      end
+      it "raises error if degree_field is blank" do
+        expect(subject.create(env)).to be false
+        expect(curation_concern.errors[:degree_field_other].first).to eq "Please provide a value for 'Other' degree field."
+      end
+    end
   end
 
   describe '#update' do
