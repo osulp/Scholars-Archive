78e8e462b30146adaf67f6ea45843409688eaeea
Josh Gum <revgum@gmail.com>
2017-08-02 08:09:28 -0700

    adds local QA hack, fixes development caching

---
+++ config/initializers/loc_generic_authority_hack.rb
@@ -0,0 +1,7 @@
+Qa::Authorities::Loc::GenericAuthority.class_eval do
+  def build_query_url(q)
+    escaped_query = "#{URI.escape(q)}*"
+    authority_fragment = Qa::Authorities::Loc.get_url_for_authority(subauthority) + URI.escape(subauthority)
+    "http://id.loc.gov/search/?q=#{escaped_query}&q=#{authority_fragment}&format=json"
+  end
+end
