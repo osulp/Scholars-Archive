89a3b953c0beebf9a493fb234e7b04aeb9ea33d4
Steve Van Tuyl <steve.vantuyl@oregonstate.edu>
2017-08-08 14:24:30 -0700

    set dataset to inherit default form behavior

--- app/forms/hyrax/dataset_form.rb
+++ app/forms/hyrax/dataset_form.rb
@@ -1,9 +1,12 @@
 # Generated via
 #  `rails generate hyrax:work Dataset`
 module Hyrax
-  class DatasetForm < DefaultForm
+  class DatasetForm < Hyrax::Forms::WorkForm
     include ::ScholarsArchive::TriplePoweredProperties::TriplePoweredForm
+    include ScholarsArchive::DatasetFormBehavior
 
     self.model_class = ::Dataset
+    self.field_metadata_service = ScholarsArchive::FormMetadataService
+
   end
 end
