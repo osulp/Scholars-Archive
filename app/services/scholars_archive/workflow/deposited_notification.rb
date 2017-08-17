cd604d4a1eb714f9c2e6222cd292760e37c3b68d
luisgreg99 <luisram90_101@hotmail.com>
2017-08-16 17:39:12 -0700

    updated deposited_notification to include a citeable url in the email

--- app/services/scholars_archive/workflow/deposited_notification.rb
+++ app/services/scholars_archive/workflow/deposited_notification.rb
@@ -8,7 +8,11 @@ module ScholarsArchive
       end
 
       def message
-        "Your deposit: #{title} (#{link_to work_id, document_path}) was approved by #{user.user_key} and is now live in ScholarsArchive@OSU. #{comment}"
+        "Your deposit: #{title} (#{link_to work_id, document_path}) was approved by #{user.user_key} and is now live in ScholarsArchive@OSU. #{comment} \n\n Citeable URL: #{citeable_url}"
+      end
+
+      def citeable_url
+        Rails.application.routes.url_helpers.url_for(:only_path => false, :action => 'show', :controller => 'hyrax/'+document.model_name.plural, :host=> Rails.application.config.action_mailer.default_url_options[:host], id: work_id)
       end
 
       def users_to_notify
