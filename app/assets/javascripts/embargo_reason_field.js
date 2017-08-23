ab73d5121d681d5ba9f322c5074eace9757b476b
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-08-23 13:28:46 -0700

    Uses element id ends with selector

--- app/assets/javascripts/embargo_reason_field.js
+++ app/assets/javascripts/embargo_reason_field.js
@@ -2,14 +2,14 @@ $( document ).ready(function() {
   $('#embargo_reason_select').change(function() {
     var val = $("#embargo_reason_select option:selected").val();
     if (val == 'Other...') {
-      $('#article_embargo_reason').removeClass('hidden');
-      $('#article_embargo_reason').val("");
+      $('[id$=embargo_reason]').removeClass('hidden');
+      $('[id$=embargo_reason]').val("");
     } else {
-      $('#article_embargo_reason').addClass('hidden');
-      $('#article_embargo_reason').val(val);
+      $('[id$=embargo_reason]').addClass('hidden');
+      $('[id$=embargo_reason]').val(val);
     }
   });
-  $('input[type="radio"]#article_visibility_embargo').on('change', function(e) {
+  $('input[type="radio"][id$=visibility_embargo]').on('change', function(e) {
     $('#embargo_reason_select').val("Intellectual Property (patent, etc.)").trigger('change');
   });
 });
