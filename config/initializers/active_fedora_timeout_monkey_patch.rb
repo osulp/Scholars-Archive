a1befa80db112913d907518c99766fc954f4db2c
luisgreg99 <luisram90_101@hotmail.com>
2017-10-05 14:49:51 -0700

    added todo note and updated active_fedora_timeout_monkey_patch

--- config/initializers/active_fedora_timeout_monkey_patch.rb
+++ config/initializers/active_fedora_timeout_monkey_patch.rb
@@ -1,13 +1,18 @@
-class ActiveFedora::Fedora
-  def request_options
-    @config[:request]
-  end
+# TODO: we can remove this file once this PR https://github.com/samvera/active_fedora/pull/1271 is released in active-fedora
 
+module ActiveFedoraOverride
   def authorized_connection
-    options = {}
-    options[:request] = request_options if request_options
-    connection = Faraday.new(host, options)
-    connection.basic_auth(user, password)
-    connection
+    super.tap do |conn|
+      if @config[:request][:timeout] && @config[:request][:timeout].to_i > 0
+        conn.options[:timeout] = @config[:request][:timeout].to_i
+      end
+      if @config[:request][:open_timeout] && @config[:request][:open_timeout].to_i > 0
+        conn.options[:open_timeout] = @config[:request][:open_timeout].to_i
+      end
+    end
   end
+end
+
+ActiveFedora::Fedora.class_eval do
+  prepend ActiveFedoraOverride unless self.class.include?(ActiveFedoraOverride)
 end
