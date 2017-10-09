87fcaccac291a9d2b8acb95216a57386bc8f2576
luisgreg99 <luisram90_101@hotmail.com>
2017-10-09 11:58:20 -0700

    updated resolrize_job and active_fedora_timeout_monkey_patch to allow more options
    updated timeout patch

--- config/initializers/active_fedora_timeout_monkey_patch.rb
+++ config/initializers/active_fedora_timeout_monkey_patch.rb
@@ -1,13 +1,16 @@
 # TODO: we can remove this file once this PR https://github.com/samvera/active_fedora/pull/1271 is released in active-fedora
 
 module ActiveFedoraOverride
+  def request_options
+    @config[:request]
+  end
+
   def authorized_connection
     super.tap do |conn|
-      if @config[:request][:timeout] && @config[:request][:timeout].to_i > 0
-        conn.options[:timeout] = @config[:request][:timeout].to_i
-      end
-      if @config[:request][:open_timeout] && @config[:request][:open_timeout].to_i > 0
-        conn.options[:open_timeout] = @config[:request][:open_timeout].to_i
+      if request_options
+        request_options.each do |option, value|
+          conn.options[option.to_sym] = value
+        end
       end
     end
   end
