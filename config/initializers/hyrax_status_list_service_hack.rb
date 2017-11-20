981c626c201ececddb1103bbfe9cbdbd57ae1ce8
luisgreg99 <luisram90_101@hotmail.com>
2017-11-20 09:27:17 -0800

    added fix to correct filtering

--- config/initializers/hyrax_status_list_service_hack.rb
+++ config/initializers/hyrax_status_list_service_hack.rb
@@ -1,8 +1,13 @@
 Hyrax::Workflow::StatusListService.class_eval do 
   def search_solr
+    if @filter_condition && @filter_condition.start_with?('-')
+      @filter_condition = "-workflow_state_name_ssim:Deposited"
+    else
+      @filter_condition = "workflow_state_name_ssim:Deposited"
+    end
     actionable_roles = roles_for_user
     logger.debug("Actionable roles for #{user.user_key} are #{actionable_roles}")
     return [] if actionable_roles.empty?
-    Hyrax::WorkRelation.new.search_with_conditions(query(actionable_roles), {df: 'actionable_workflow_roles', defType: 'edismax', rows: 1000})
+    Hyrax::WorkRelation.new.search_with_conditions(query(actionable_roles), {df: 'actionable_workflow_roles', defType: 'edismax', rows: 1000, method: :post, fq: @filter_condition })
   end
 end
