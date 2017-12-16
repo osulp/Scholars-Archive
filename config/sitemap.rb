5857339cdb3f074ffb82361b1fe963acb656b6b8
luisgreg99 <luisram90_101@hotmail.com>
2017-12-15 16:39:30 -0800

    set default public_path to point to shared folder

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,7 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://ir.library.oregonstate.edu"
 SitemapGenerator::Sitemap.create_index = true
+SitemapGenerator::Sitemap.public_path = '/data0/hydra/shared/public/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemap/'
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
