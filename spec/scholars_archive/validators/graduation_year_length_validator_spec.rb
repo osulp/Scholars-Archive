6b61bab3baba651b2a3d41d75e8062e00553d67d
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-01-26 11:19:05 -0800

    Adds tests

---
+++ spec/scholars_archive/validators/graduation_year_length_validator_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ScholarsArchive::Validators::GraduationYearLengthValidator do
+  describe "#validate" do
+    let(:validator) { described_class.new }
+    let(:correct_graduation_year) { "1925" }
+    let(:incorrect_graduation_year) { "1925blah" }
+    let(:record) {}
+
+    context "When an etd has an improper graduation year" do
+      let(:record) {UndergraduateThesisOrProject.new(graduation_year: incorrect_graduation_year)}
+      it "set an error on the record" do
+        validator.validate(record)
+        expect(record.errors["graduation_year"]).to eq ["Please ensure you a year is used in this field (e.g 1901)"]
+      end
+    end
+    context "When an etd has a proper graduation year" do
+      let(:record) {UndergraduateThesisOrProject.new(graduation_year: correct_graduation_year)}
+      it "set an error on the record" do
+        validator.validate(record)
+        expect(record.errors["graduation_year"]).to be_blank
+      end
+    end
+
+  end
+end
