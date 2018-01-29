0557fd8e876441e0426e584772361fff5129f038
Josh Gum <revgum@gmail.com>
2018-01-29 13:55:05 -0800

    reworks external/search linking and consistent styling (#1188)
    extracts the url from a label containing a url
    
    adds link color in abstracts presenter
    
    adds rights statements and licenses logic to external linking

---
+++ app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
@@ -0,0 +1,13 @@
+module Hyrax
+  module Renderers
+    class ExternalLinkAttributeRenderer < AttributeRenderer
+      private
+
+        def li_value(value)
+          auto_link(value) do |link|
+            "#{link}&nbsp;&nbsp;<span class='glyphicon glyphicon-new-window'></span>"
+          end
+        end
+    end
+  end
+end
---
+++ spec/renderers/hyrax/renderers/external_link_attribute_renderer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+RSpec.describe Hyrax::Renderers::ExternalLinkAttributeRenderer do
+  let(:field) { :name }
+  let(:renderer) { described_class.new(field, ['http://example.com']) }
+
+  describe "#attribute_to_html" do
+    subject { Nokogiri::HTML(renderer.render) }
+
+    let(:expected) { Nokogiri::HTML(tr_content) }
+
+    let(:tr_content) do
+      "<tr><th>Name</th>\n" \
+       "<td><ul class='tabular'>" \
+       "<li class=\"attribute name\">"\
+       "<a href=\"http://example.com\">"\
+       "http://example.com&nbsp;&nbsp;<span class='glyphicon glyphicon-new-window'></span></a></li>"\
+       "</ul></td></tr>"
+    end
+
+    it { expect(subject).to be_equivalent_to(expected) }
+  end
+end
