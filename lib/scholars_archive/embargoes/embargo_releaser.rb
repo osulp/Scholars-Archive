6af86c7699b8090c8a85085e66a0fe5ce15bda97
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2018-01-31 08:46:41 -0800

    Adds code for expiring embargoes (#1197)
    * Adds code for expiring embargoes
    
    * makes default empty, with first choice at top prompting to choose a reason.
    
    * Adds title of work

---
+++ lib/scholars_archive/embargoes/embargo_releaser.rb
@@ -0,0 +1,22 @@
+module ScholarsArchive::Embargoes
+  class EmbargoReleaser
+    extend Hyrax::EmbargoHelper
+    
+    def self.expire_embargoes
+      expired_embargoes = self.find_all_expired_embargoes
+      expired_embargoes.each do |embargo|
+        work = ActiveFedora::Base.find(embargo.solr_document[:id])
+        puts "Work thats being expired"
+        puts "Title: #{work.title}"
+        Hyrax::Actors::EmbargoActor.new(work).destroy
+      end
+      puts "Expired #{expired_embargoes.length} embargoes"
+    end
+
+    private
+
+    def self.find_all_expired_embargoes
+      assets_with_expired_embargoes
+    end
+  end
+end
