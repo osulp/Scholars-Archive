73a95543e20408868f720528273a01b221c52da0
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-01-31 13:46:37 -0800

    Default datatable pick set to 50

--- app/assets/javascripts/default_datatable_order.js
+++ app/assets/javascripts/default_datatable_order.js
@@ -2,22 +2,24 @@ $(document).on('ready turbolinks:load', function() {
     review_table_selector = '#under-review.tab-pane .datatable'
     if ( $.fn.dataTable.isDataTable(review_table_selector) ) {
         review_table = $(review_table_selector).DataTable();
-        review_table.order([ 3, "desc" ]).draw();
+        review_table.order([ 3, "desc" ]).pageLength(50).draw();
     }
     else {
         review_table = $(review_table_selector).DataTable( {
-            "order": [[ 3, "desc" ]]
+            "order": [[ 3, "desc" ]],
+            "pageLength": 50
         } );
     }
 
     published_table_selector = '#published.tab-pane .datatable'
     if ( $.fn.dataTable.isDataTable(published_table_selector) ) {
         published_table = $(published_table_selector).DataTable();
-        published_table.order([ 3, "desc" ]).draw();
+        published_table.order([ 3, "desc" ]).pageLength(50).draw();
     }
     else {
         published_table = $(published_table_selector).DataTable( {
-            "order": [[ 3, "desc" ]]
+            "order": [[ 3, "desc" ]],
+            "pageLength": 50
         } );
     }
 } );
