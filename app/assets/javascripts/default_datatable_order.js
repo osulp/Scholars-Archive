8687ec2718ffeb106e38c90e387dd9632ffd7dca
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2018-02-12 12:48:28 -0800

    follow api for datatable in hyrax (#1231)

--- app/assets/javascripts/default_datatable_order.js
+++ app/assets/javascripts/default_datatable_order.js
@@ -2,7 +2,8 @@ $(document).on('ready turbolinks:load', function() {
     review_table_selector = '#under-review.tab-pane .datatable'
     if ( $.fn.dataTable.isDataTable(review_table_selector) ) {
         review_table = $(review_table_selector).DataTable();
-        review_table.order([ 3, "desc" ]).pageLength(50).draw();
+        review_table.order([ 3, "desc" ]).draw();
+        review_table.page.len(50).draw();
     }
     else {
         review_table = $(review_table_selector).DataTable( {
@@ -14,7 +15,8 @@ $(document).on('ready turbolinks:load', function() {
     published_table_selector = '#published.tab-pane .datatable'
     if ( $.fn.dataTable.isDataTable(published_table_selector) ) {
         published_table = $(published_table_selector).DataTable();
-        published_table.order([ 3, "desc" ]).pageLength(50).draw();
+        published_table.order([ 3, "desc" ]).draw();
+        published_table.page.len(50).draw();
     }
     else {
         published_table = $(published_table_selector).DataTable( {
