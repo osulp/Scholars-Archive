9e88112b73b8d30a8f1f861d75066817303c4bb5
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-02-19 10:47:53 -0800

    Fixes namespacing for object

--- spec/presenters/hyrax/purchased_e_resource_presenter_spec.rb
+++ spec/presenters/hyrax/purchased_e_resource_presenter_spec.rb
@@ -2,7 +2,7 @@
 #  `rails generate hyrax:work PurchasedEResource`
 require 'rails_helper'
 
-RSpec.describe Hyrax::PurchasedEResourcePresenter do
+RSpec.describe PurchasedEResourcePresenter do
   it "has tests" do
     skip "Add your tests here"
   end
