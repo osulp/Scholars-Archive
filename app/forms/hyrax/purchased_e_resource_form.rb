6b30e7d560e1b6646f5062e469350157d59b2e6d
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-02-20 16:10:33 -0800

    removes advisor and comitteemember fields

--- app/forms/hyrax/purchased_e_resource_form.rb
+++ app/forms/hyrax/purchased_e_resource_form.rb
@@ -11,7 +11,7 @@ module Hyrax
     self.model_class = ::PurchasedEResource
 
     def primary_terms
-      [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :bibliographic_citation, :is_referenced_by, :has_journal, :has_volume, :has_number, :conference_location, :conference_name, :conference_section, :editor, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :contributor_advisor, :contributor_committeemember, :bibliographic_citation, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | super
+      [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :bibliographic_citation, :is_referenced_by, :has_journal, :has_volume, :has_number, :conference_location, :conference_name, :conference_section, :editor, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :bibliographic_citation, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | super
     end
   end
 end
