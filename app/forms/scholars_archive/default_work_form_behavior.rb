c6c1000b40f7d5a437a45e8504c37c487bf16ab0
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-04-10 12:55:44 -0700

    Adds hidden values to the end for admin users.

--- app/forms/scholars_archive/default_work_form_behavior.rb
+++ app/forms/scholars_archive/default_work_form_behavior.rb
@@ -24,7 +24,11 @@ module ScholarsArchive
       end
 
       def secondary_terms
-        [:nested_related_items, :hydrologic_unit_code, :geo_section, :funding_statement, :publisher, :peerreviewed, :conference_location, :conference_name, :conference_section, :language, :file_format, :file_extent, :digitization_spec, :replaces, :additional_information, :isbn, :issn]
+        if current_ability.current_user.admin?
+          [:nested_related_items, :hydrologic_unit_code, :geo_section, :funding_statement, :publisher, :peerreviewed, :conference_location, :conference_name, :conference_section, :language, :file_format, :file_extent, :digitization_spec, :replaces, :additional_information, :isbn, :issn, :keyword, :source, :funding_body, :dspace_community, :dspace_collection]
+        else
+          [:nested_related_items, :hydrologic_unit_code, :geo_section, :funding_statement, :publisher, :peerreviewed, :conference_location, :conference_name, :conference_section, :language, :file_format, :file_extent, :digitization_spec, :replaces, :additional_information, :isbn, :issn]
+        end
       end
 
       def self.date_terms
