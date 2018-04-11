198c9905f617b976436e3bf92d57cefe72324a1c
Josh Gum <revgum@gmail.com>
2018-04-11 15:25:12 -0700

    updates style and positioning of information for footer

--- config/initializers/application_revision.rb
+++ config/initializers/application_revision.rb
@@ -4,7 +4,8 @@ begin
   lines = File.open(revision_file_path).to_a.map{ |line| line.chomp }
   last_revision = lines.last.split(' ')[3].gsub(')','')
   last_branch = lines.last.split(' ')[1]
-  revision = "Deployed #{last_branch}@#{last_revision}"
+  last_deployed = lines.last.split(' ')[7]
+  revision = "Deployed #{last_branch}@#{last_revision.last(6)}@#{last_deployed}"
   puts "Deploy revision #{revision} parsed from #{revision_file_path} file."
 rescue
   puts "ERROR: Wasn't able to parse #{revision_file_path} file."
