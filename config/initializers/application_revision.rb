72cafc71aa57703473700bad304a59c22d9d3d4f
Josh Gum <revgum@gmail.com>
2018-04-12 11:27:22 -0700

    footer details (#1324)
    * application revision partial
    
    * updates style and positioning of information for footer
    
    * footer powered by hyrax

---
+++ config/initializers/application_revision.rb
@@ -0,0 +1,14 @@
+revision = ''
+revision_file_path = File.join(Rails.root, '..', 'revisions.log')
+begin
+  lines = File.open(revision_file_path).to_a.map{ |line| line.chomp }
+  last_revision = lines.last.split(' ')[3].gsub(')','')
+  last_branch = lines.last.split(' ')[1]
+  last_deployed = lines.last.split(' ')[7]
+  revision = "Deployed #{last_branch}@#{last_revision.last(6)}@#{last_deployed}"
+  puts "Deploy revision #{revision} parsed from #{revision_file_path} file."
+rescue
+  puts "ERROR: Wasn't able to parse #{revision_file_path} file."
+end
+
+ENV['SCHOLARSARCHIVE_DEPLOYED_REVISION'] = revision
