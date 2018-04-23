b7415e6afecd92d891441a14872ff043de2750a6
Josh Gum <revgum@gmail.com>
2018-04-23 13:25:30 -0700

    adds missing ApplicationJob

---
+++ app/jobs/application_job.rb
@@ -0,0 +1,5 @@
+# A common base class for all ScholarsArchive jobs.
+# This allows downstream applications to manipulate all the ScholarsArchive jobs by
+# including modules on this class.
+class ApplicationJob < ActiveJob::Base
+end
