c20f5d2edd3b6f7f6ba252d513db2b6026d23013
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-04-26 13:12:47 -0700

    more rows

--- spec/services/scholars_archive/parent_query_service_spec.rb
+++ spec/services/scholars_archive/parent_query_service_spec.rb
@@ -14,7 +14,7 @@ describe ScholarsArchive::ParentQueryService do
   let(:child_id) {"child_id"}
   describe "#query_parents_for_id" do
     before do
-      allow(ActiveFedora::SolrService).to receive(:get).with("member_ids_ssim:#{child_id}", rows: 10000).and_return(parent_doc)
+      allow(ActiveFedora::SolrService).to receive(:get).with("member_ids_ssim:#{child_id}", rows: 100000).and_return(parent_doc)
     end
     context "when a child work exists with a parent" do
       it "returns the parent works" do
@@ -22,7 +22,7 @@ describe ScholarsArchive::ParentQueryService do
       end
     end
     before do
-      allow(ActiveFedora::SolrService).to receive(:get).with("member_ids_ssim:#{child_id}", rows: 10000).and_return(empty_results)
+      allow(ActiveFedora::SolrService).to receive(:get).with("member_ids_ssim:#{child_id}", rows: 100000).and_return(empty_results)
     end
     context "when a work exists without a parent" do
       it "returns an empty array" do
