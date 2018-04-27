9d5aad9f45b28651f11a121841a3a1ca8fe376e6
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2018-04-27 15:14:09 -0700

    Query parent and display on show page (#1367)
    * Query parent and display on show page
    
    * more rows
    
    * Fix spec again
    
    * use symbol
    
    * fixes tests
    
    * Fix overrided file
    
    * with indifferent access
    
    * fixes test more
    
    * Remove byebug

---
+++ app/services/scholars_archive/parent_query_service.rb
@@ -0,0 +1,7 @@
+module ScholarsArchive
+  class ParentQueryService
+    def self.query_parents_for_id(child_id)
+      ActiveFedora::SolrService.get("member_ids_ssim:#{child_id}", :rows => 100000)["response"]["docs"]
+    end
+  end
+end
