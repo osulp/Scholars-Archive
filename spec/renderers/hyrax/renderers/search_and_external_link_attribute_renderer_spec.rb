9b891ae0498ae01959c4840b948165719a7c91bf
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-05-16 14:48:19 -0700

    Fix tests more

--- spec/renderers/hyrax/renderers/search_and_external_link_attribute_renderer_spec.rb
+++ spec/renderers/hyrax/renderers/search_and_external_link_attribute_renderer_spec.rb
@@ -9,14 +9,13 @@ RSpec.describe Hyrax::Renderers::SearchAndExternalLinkAttributeRenderer do
     let(:uri) { "http://test.org/ns/TestSubject/TestLabel" }
     let(:label) { "test label" }
     let(:label_uris) { {'label' => label, 'uri' => uri}}
-    let(:linked_label) { "label$uri" }
     let(:label_q) { "test+label" }
     let(:renderer) { described_class.new(field, [label_uris.to_s], search_field: 'academic_affiliation_label') }
     let(:tr_content) do
       %(
       <tr>
       <th>Search and external link</th>
-      <td><ul class="tabular"><li class="attribute attribute-search_and_external_link"><a href="/catalog?q=#{label_q}&search_field=academic_affiliation_label">#{label}</a><a aria-label="Open link in new window" class="btn" href="#{uri}" target="_blank"><span class="glyphicon glyphicon-new-window"></span></a></li></ul></td>
+      <td><ul class="tabular"><li class="attribute search_and_external_link"><a href="/catalog?q=#{label_q}&search_field=academic_affiliation_label">#{label}</a><a aria-label="Open link in new window" class="btn" href="#{uri}" target="_blank"><span class="glyphicon glyphicon-new-window"></span></a></li></ul></td>
       </tr>
       )
     end
@@ -25,27 +24,26 @@ RSpec.describe Hyrax::Renderers::SearchAndExternalLinkAttributeRenderer do
     }
 
     context "with a URI label" do
-      let(:renderer) { described_class.new(field, [linked_label.to_s], search_field: 'based_near_label') }
-      let(:linked_label_label) { "label" }
-      let(:linked_label_uri) { "uri" }
-      let(:expected) { Nokogiri::HTML(tr_content) }
-      let(:tr_content) do
-        %(
-      <tr>
-      <th>Search and external link</th>
-      <td><ul class="tabular"><li class="attribute attribute-search_and_external_link"><a href="/catalog?q=#{linked_label_label}&search_field=academic_affiliation_label">#{linked_label_label}</a><a aria-label="Open link in new window" class="btn" href="#{linked_label_uri}" target="_blank"><span class="glyphicon glyphicon-new-window"></span></a></li></ul></td>
-      </tr>
-        )
-      end
+      let(:label) { uri }
+      let(:label_uris) { uri }
+      let(:label_q) { CGI.escape(uri) }
       it {
         expect(subject).to be_equivalent_to(expected)
       }
     end
 
-    context "with a linked_label" do
-      let(:label) { uri }
-      let(:label_uris) { uri }
-      let(:label_q) { CGI.escape(uri) }
+    context "with a linked label" do
+      let(:linked_data_string) { "label$uri" }
+      let(:linked_data_label) { "label" }
+      let(:linked_data_uri) { "uri" }
+      let(:tr_content) do
+        %(
+      <tr>
+      <th>Search and external link</th>
+      <td><ul class="tabular"><li class="attribute search_and_external_link"><a href="/catalog?q=#{linked_data_label}&search_field=based_near_label">#{linked_data_label}</a><a aria-label="Open link in new window" class="btn" href="#{linked_data_uri}" target="_blank"><span class="glyphicon glyphicon-new-window"></span></a></li></ul></td>
+      </tr>
+        )
+      end
       it {
         expect(subject).to be_equivalent_to(expected)
       }
@@ -59,7 +57,7 @@ RSpec.describe Hyrax::Renderers::SearchAndExternalLinkAttributeRenderer do
         %(
         <tr>
         <th>Search and external link</th>
-        <td><ul class="tabular"><li class="attribute attribute-search_and_external_link"><a href="/catalog?q=#{label_q}&search_field=academic_affiliation_label">#{label}</a></li></ul></td>
+        <td><ul class="tabular"><li class="attribute search_and_external_link"><a href="/catalog?q=#{label_q}&search_field=academic_affiliation_label">#{label}</a></li></ul></td>
         </tr>
         )
       end
