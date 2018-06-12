2469ffd890af84b5052690bcc95c60f5b445fde4
Josh Gum <revgum@gmail.com>
2018-06-12 10:46:55 -0700

    reworks http based authentication to be multi-use

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,18 @@ class ApplicationController < ActionController::Base
   include Hyrax::ThemedLayoutController
   with_themed_layout '1_column'
 
-  protect_from_forgery with: :exception
-
   # Hyrax 2.1 migration
   skip_after_action :discard_flash_if_xhr
 
-  before_action :check_d2h_http_header_auth
+  protect_from_forgery with: :exception, unless: :http_header_auth?
+  skip_before_action :verify_authenticity_token, unless: :http_header_auth?
+  before_action :http_header_auth_login
   before_action :update_from_person_api
 
+  ##
+  # Attempt to query and update the current user information from the OSU directory
+  # API. This helps to maintain details of the user type (student, employee, etc)
+  # which can be leveraged to improve the usability for different types of users.
   def update_from_person_api
     if user_signed_in?
       begin
@@ -28,13 +32,27 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def check_d2h_http_header_auth
-    if !user_signed_in? && request.headers.key?('HTTP_D2H_AUTHENTICATION')
-      email, token = request.headers['HTTP_D2H_AUTHENTICATION'].split('|')
-      if token === ENV['HTTP_D2H_AUTHENTICATION_TOKEN'] && email === ENV['HTTP_D2H_AUTHENTICATION_USERNAME']
-        u = User.where(email: email).first
+  ##
+  # Detect if the appropriate http header exists and if it has a username and token
+  # to match the local_env.yml configuration. Used to bypass form token authentication
+  # and to verify that the http request in question is providing legit credentials.
+  def http_header_auth?
+    return false unless request.headers.key?('HTTP_API_AUTHENTICATION')
+    credentials = api_credentials(request.headers)
+    credentials[:header][:token] == credentials[:config][:token] && credentials[:header][:username] == credentials[:config][:username]
+  end
+
+  ##
+  # Bypass typical CAS authentication by querying the configured user and setting
+  # it as authenticated. This allows for API requests to flow through without requiring
+  # a CAS authentication to happen beforehand.
+  def http_header_auth_login
+    if !user_signed_in? && request.headers.key?('HTTP_API_AUTHENTICATION')
+      credentials = api_credentials(request.headers)
+      if http_header_auth?
+        u = User.where(email: credentials[:config][:username]).first
         sign_in :user, u
-        redirect_to root_path
+        authenticate_user!
       else
         warden.custom_failure!
         render json: 'Unable to authenticate user.', status: 422
@@ -42,6 +60,29 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  ##
+  # Fetch the token and username from the local_env.yml (ENV) and from
+  # the provided http header.
+  # @param [HTTP::Headers] headers - the headers provided in this request
+  # @return [Hash] - the header and configuration username and token values
+  def api_credentials(headers)
+    token = ENV.fetch('HTTP_API_AUTHENTICATION_TOKEN', nil)
+    username = ENV.fetch('HTTP_API_AUTHENTICATION_USERNAME', nil)
+    raise 'Invalid or missing configurations for HTTP API authentication' unless token && username
+    {
+      header: {
+        username: headers['HTTP_API_AUTHENTICATION'].split('|')[0],
+        token: headers['HTTP_API_AUTHENTICATION'].split('|')[1]
+      },
+      config: {
+        username: username,
+        token: token
+      }
+    }
+  end
+
+  ##
+  # Force https for production
   def default_url_options
     if Rails.env == "production"
       super.merge(protocol: :https) if Rails.env == 'production'
@@ -49,5 +90,4 @@ class ApplicationController < ActionController::Base
       super
     end
   end
-
 end
