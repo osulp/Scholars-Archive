85f3d1908bf18ba3412e9a933b9d95c6606b3b5d
Josh Gum <revgum@gmail.com>
2018-06-19 13:09:08 -0700

    fixing migration for pre-existing table in production

--- db/migrate/20180426200657_create_job_io_wrappers.hyrax.rb
+++ db/migrate/20180426200657_create_job_io_wrappers.hyrax.rb
@@ -1,15 +1,20 @@
 class CreateJobIoWrappers < ActiveRecord::Migration[5.0]
   def change
-    create_table :job_io_wrappers do |t|
-      t.references :user
-      t.references :uploaded_file
-      t.string :file_set_id
-      t.string :mime_type
-      t.string :original_name
-      t.string :path
-      t.string :relation
+    # A hack put in place because this table was established in earlier releases of
+    # Hyrax, then again more recently.. so the table existed in this state in our production
+    # database already and would fail to migrate properly.
+    unless ActiveRecord::Base.connection.data_source_exists? 'job_io_wrappers'
+      create_table :job_io_wrappers do |t|
+        t.references :user
+        t.references :uploaded_file
+        t.string :file_set_id
+        t.string :mime_type
+        t.string :original_name
+        t.string :path
+        t.string :relation
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
