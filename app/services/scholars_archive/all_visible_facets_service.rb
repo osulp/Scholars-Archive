def6fc11bbe98e09d90f499c943c33f05b1d52d0
Josh Gum <revgum@gmail.com>
2018-06-21 13:45:24 -0700

    refactors for clarity and testing

--- app/services/scholars_archive/all_visible_facets_service.rb
+++ app/services/scholars_archive/all_visible_facets_service.rb
@@ -1,12 +1,11 @@
 module ScholarsArchive
   class AllVisibleFacetsService
-    def call(current_user, facet)
-      facets('*:* AND -suppressed_bsi:true', facet.key)
+    def call(facet)
+      facets = solr_facets('*:* AND -suppressed_bsi:true', facet.key)
+      facets.keys
     end
 
-    private
-
-    def facets(query_string, facet_field)
+    def solr_facets(query_string, facet_field)
       solr_connection = RSolr::Ext.connect(url: ActiveFedora.solr_config[:url])
       params = {
         rows: 0,
@@ -15,8 +14,7 @@ module ScholarsArchive
         "facet.limit" => 100000
       }
       response = solr_connection.find(params, method: :get)
-      results = Hash[*response['facet_counts']['facet_fields'][facet_field]]
-      results.keys
+      Hash[*response['facet_counts']['facet_fields'][facet_field]]
     end
   end
 end
--- spec/services/scholars_archive/all_visible_facets_service_spec.rb
+++ spec/services/scholars_archive/all_visible_facets_service_spec.rb
@@ -4,15 +4,16 @@ describe ScholarsArchive::AllVisibleFacetsService do
   let(:service) { described_class.new }
   let(:facet) { double("facet") }
   let(:facet_results) { ["bob", "ross", "banana"] }
+  let(:solr_results) { {"bob" => 1, "ross" => 1, "banana" => 1} }
   let(:facet_key) { "creator_sim" }
 
   describe "#call" do
     before do
       allow(facet).to receive(:key).and_return(facet_key)
-      allow(service).to receive(:facets).with(anything(), anything()).and_return(facet_results)
+      allow(service).to receive(:solr_facets).with(anything(), anything()).and_return(solr_results)
     end
     it "returns all unique creators in an array" do
-      expect(service.call(user, facet)).to eq (admin_facet_results)
+      expect(service.call(facet)).to eq (facet_results)
     end
   end
 end
