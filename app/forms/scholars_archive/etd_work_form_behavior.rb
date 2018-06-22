ae6082d5d49379b661fe4e69a2b853acf905ea8d
Ryan Wick <ryanwick@gmail.com>
2018-06-22 13:16:57 -0700

    Remove academic affiliation as required field. (#1463)

--- app/forms/scholars_archive/etd_work_form_behavior.rb
+++ app/forms/scholars_archive/etd_work_form_behavior.rb
@@ -6,7 +6,7 @@ module ScholarsArchive
       attr_accessor :degree_grantors_other
 
       self.terms += [:degree_grantors, :contributor_advisor, :contributor_committeemember, :graduation_year, :degree_discipline]
-      self.required_fields += [:degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :academic_affiliation]
+      self.required_fields += [:degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year]
 
       def primary_terms
         [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :dates_section, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :contributor_advisor, :contributor_committeemember, :bibliographic_citation, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | super
