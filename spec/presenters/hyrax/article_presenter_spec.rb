3274e679e1f372bdf82cb5cc6ade653006cfd9e7
Josh Gum <revgum@gmail.com>
2018-07-25 08:24:20 -0700

    fixes test requiring wos uid to be included during initialization

--- spec/presenters/hyrax/article_presenter_spec.rb
+++ spec/presenters/hyrax/article_presenter_spec.rb
@@ -1,23 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ArticlePresenter do
   let(:solr_document) { SolrDocument.new(attributes) }
-  let(:ability) { double "Ability" }
+  let(:ability) { double 'Ability' }
   let(:presenter) { described_class.new(solr_document, ability) }
   let(:attributes) { file.to_solr }
   let(:file) do
     Article.new(
-          id: '123abc',
-          title: ["File title"],
-          depositor: user.user_key,
-          label: "filename.tif")
+      id: '123abc',
+      title: ['File title'],
+      depositor: user.user_key,
+      label: 'filename.tif',
+      web_of_science_uid: 'test'
+    )
   end
-  let(:user) { double(user_key: 'sarah')}
+  let(:user) { double(user_key: 'sarah') }
   let(:solr_properties) do
-    ["resource_type", "editor", "has_volume", "has_number", "conference_location", "conference_name", "conference_section", "has_journal", "is_referenced_by", "isbn", "web_of_science_uid"]
+    %w[resource_type editor has_volume has_number conference_location conference_name conference_section has_journal is_referenced_by isbn web_of_science_uid]
   end
   subject { presenter }
-  it "delegates to the solr_document" do
+
+  it 'delegates to the solr_document' do
     solr_properties.each do |property|
       expect(solr_document).to receive(property.to_sym)
       presenter.send(property)
