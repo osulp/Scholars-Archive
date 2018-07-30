f2736c0e1c436ac24b745b4f2b4e8a3a43594169
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-07-30 15:26:19 -0700

    Update embargo task to log and use embargo actor

--- lib/scholars_archive/embargoes/embargo_releaser.rb
+++ lib/scholars_archive/embargoes/embargo_releaser.rb
@@ -4,15 +4,43 @@ module ScholarsArchive::Embargoes
       expired_embargoes = Hyrax::EmbargoService.assets_with_expired_embargoes
       expired_embargoes.each do |embargo|
         work = ActiveFedora::Base.find(embargo.solr_document[:id])
-        puts "Expired embargo for #{embargo.solr_document[:id]}"
-        work.embargo_visibility!
-        work.deactivate_embargo!
-        work.embargo.save!
-        # Having to skip validation because some migrated works still have broken/invalid metadata that won't necessarily
-        # always pass standard validation.
-        work.save!(validate: false)
+        expired = true
+
+        #attempt to expire embargo
+        begin
+          Hyrax::Actors::EmbargoActor.new(work).destroy
+        rescue
+          file_name = Date.today.to_s + "-embargo-debug.log"
+          logger = Logger.new(File.join(Rails.root, 'log', file_name))
+          logger.warning("Couldnt destroy embargo for #{work.id}")
+          expired = false
+        end
+
+        if work.file_set? && expired
+          work.visibility = work.to_solr["visibility_after_embargo_ssim"]
+
+          #Attempt to save the file_set
+          begin
+            work.save!
+          rescue
+            file_name = Date.today.to_s + "-embargo-file_set-debug.log"
+            logger = Logger.new(File.join(Rails.root, 'log', file_name))
+            logger.warning("Couldnt destroy embargo for #{work.id}")
+          end
+
+        elsif !work.file_set? && expired
+
+          #Attempt to copy visibility
+          begin
+            work.copy_visibility_to_files
+          rescue
+            file_name = Date.today.to_s + "-embargo-copy_vis-debug.log"
+            logger = Logger.new(File.join(Rails.root, 'log', file_name))
+            logger.warning("Couldnt destroy embargo for #{work.id}")
+          end
+
+        end
       end
-      puts "Expired #{expired_embargoes.length} embargoes"
     end
   end
 end
