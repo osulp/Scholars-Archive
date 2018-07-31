0884b81fb2a9c8d9f2c647700edde48c157c06ca
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-07-31 10:24:07 -0700

    Single begin rescue and logger

--- lib/scholars_archive/embargoes/embargo_releaser.rb
+++ lib/scholars_archive/embargoes/embargo_releaser.rb
@@ -1,47 +1,28 @@
 module ScholarsArchive::Embargoes
   class EmbargoReleaser
     def self.expire_embargoes
+
+      file_name = "#{Date.today}-embargo-release.log"
+      logger = Logger.new(File.join(Rails.root, 'log', file_name))
+
       expired_embargoes = Hyrax::EmbargoService.assets_with_expired_embargoes
       expired_embargoes.each do |embargo|
         work = ActiveFedora::Base.find(embargo.solr_document[:id])
-        expired = true
-
-        #attempt to expire embargo
+        logger.warning("Processing work id: #{work.id}")
         begin
-          Hyrax::Actors::EmbargoActor.new(work).destroy
-        rescue => e
-          file_name = Date.today.to_s + "-embargo-debug.log"
-          logger = Logger.new(File.join(Rails.root, 'log', file_name))
-          logger.warning("Couldnt destroy embargo for #{work.id}")
-          logger.warning ([e.message]+e.backtrace).join("\n")
-          expired = false
-        end
-
-        if work.file_set? && expired
-          work.visibility = work.to_solr["visibility_after_embargo_ssim"]
-
-          #Attempt to save the file_set
-          begin
-            work.save!
-          rescue => e
-            file_name = Date.today.to_s + "-embargo-file_set-debug.log"
-            logger = Logger.new(File.join(Rails.root, 'log', file_name))
-            logger.warning("Couldnt update fileset #{work.id}")
-            logger.warning ([e.message]+e.backtrace).join("\n")
-          end
-
-        elsif !work.file_set? && expired
-
-          #Attempt to copy visibility
-          begin
+          work.embargo_visibility!
+          work.deactivate_embargo!
+          work.embargo.save!
+          work.save!(validate: false)
+          if work.file_set?
+            work.visibility = work.to_solr["visibility_after_embargo_ssim"]
+            work.save!(validate: false)
+          elsif !work.file_set?
             work.copy_visibility_to_files
-          rescue => e
-            file_name = Date.today.to_s + "-embargo-copy_vis-debug.log"
-            logger = Logger.new(File.join(Rails.root, 'log', file_name))
-            logger.warning("Couldnt copy visibility for #{work.id}")
-            logger.warning ([e.message]+e.backtrace).join("\n")
           end
-
+        rescue => e
+          logger.warning("Couldnt process #{work.id}")
+          logger.warning ([e.message]+e.backtrace).join("\n")
         end
       end
     end
