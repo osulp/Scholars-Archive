aa8e291e98ae41f37115b15883104d05ba9532ec
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2018-07-31 10:55:45 -0700

    [WIP] Update embargo task to log and use embargo actor (#1538)
    * Update embargo task to log and use embargo actor
    
    * include backtrace
    
    * Single begin rescue and logger

--- lib/scholars_archive/embargoes/embargo_releaser.rb
+++ lib/scholars_archive/embargoes/embargo_releaser.rb
@@ -1,18 +1,30 @@
 module ScholarsArchive::Embargoes
   class EmbargoReleaser
     def self.expire_embargoes
+
+      file_name = "#{Date.today}-embargo-release.log"
+      logger = Logger.new(File.join(Rails.root, 'log', file_name))
+
       expired_embargoes = Hyrax::EmbargoService.assets_with_expired_embargoes
       expired_embargoes.each do |embargo|
         work = ActiveFedora::Base.find(embargo.solr_document[:id])
-        puts "Expired embargo for #{embargo.solr_document[:id]}"
-        work.embargo_visibility!
-        work.deactivate_embargo!
-        work.embargo.save!
-        # Having to skip validation because some migrated works still have broken/invalid metadata that won't necessarily
-        # always pass standard validation.
-        work.save!(validate: false)
+        logger.warning("Processing work id: #{work.id}")
+        begin
+          work.embargo_visibility!
+          work.deactivate_embargo!
+          work.embargo.save!
+          work.save!(validate: false)
+          if work.file_set?
+            work.visibility = work.to_solr["visibility_after_embargo_ssim"]
+            work.save!(validate: false)
+          elsif !work.file_set?
+            work.copy_visibility_to_files
+          end
+        rescue => e
+          logger.warning("Couldnt process #{work.id}")
+          logger.warning ([e.message]+e.backtrace).join("\n")
+        end
       end
-      puts "Expired #{expired_embargoes.length} embargoes"
     end
   end
 end
