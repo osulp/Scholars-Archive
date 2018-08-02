aacb8a1c87bf5c2d8848244caa113a5fe74575c3
Ryan Wick <ryan.wick@oregonstate.edu>
2018-08-02 15:43:51 -0700

    Expand more info tooltip to include links starting with http

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -3,11 +3,13 @@ $(document).on('ready turbolinks:load', function() {
     placement: 'top',
     title: 'Search for this term in SA@OSU'
   };
-
-  $('.metadata > .table a').not('.btn').tooltip(search_options);
-  $('li.attribute a').not('.btn').tooltip(search_options);
-  $('li.attribute a.btn').tooltip({
+  var more_information = {
     placement: 'top',
     title: 'More information about this item'
-  });
+  };
+
+  $('.metadata > .table a').not('.btn').tooltip(search_options);
+  $('li.attribute a').not('.btn').not('[href^="http"]').tooltip(search_options);
+  $('li.attribute a.btn').tooltip(more_information);
+  $('li.attribute a[href^="http"]').tooltip(more_information);
 });
