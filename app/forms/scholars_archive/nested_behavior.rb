5dcdfe58c446d8b21bf781459ff1c58c30c607d9
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2018-08-03 08:23:41 -0700

    updated model behavior to enable the attributes to be persisted

--- app/forms/scholars_archive/nested_behavior.rb
+++ app/forms/scholars_archive/nested_behavior.rb
@@ -6,7 +6,7 @@ module ScholarsArchive
 
       delegate :nested_geo_attributes=, :to => :model
       delegate :nested_related_items_attributes=, :to => :model
-      delegate :nested_order_creator_attributes=, :to => :model
+      delegate :nested_ordered_creator_attributes=, :to => :model
 
       def initialize_fields
         model.nested_geo.build
--- app/inputs/multi_value_ordered_input.rb
+++ app/inputs/multi_value_ordered_input.rb
@@ -23,8 +23,7 @@ class MultiValueOrderedInput < MultiValueInput
     "
       <li class='field-wrapper dd-item'>
         <div class='dd-handle dd3-handle'></div>\n
-        #{yield}\n
-        <input type='hidden' name='#{nested_field_name(value, index)}' id='#{nested_field_id(value, index)}'' />
+        #{yield}
       </li>\n
     "
   end
@@ -39,37 +38,38 @@ class MultiValueOrderedInput < MultiValueInput
   private
 
   def build_field(value, index)
-    unless value.is_a?(String)
-      if value.new_record?
-        index = value.object_id
-      end
-      label_options = build_label_options(value, index)
-      input_label = @builder.text_field(:nested_ordered_creator_label, label_options)
-
-      unless value.new_record?
-        id_options = build_id_options(value.id, index)
-        input_id = @builder.text_field(:id, id_options)
-      end
-
-      if value.destroy_item == true
-        destroy_options = build_destroy_options(value.id, index)
-        destroy_input = @builder.text_field(:_destroy, destroy_options)
-      end
-
-      help_block = nested_item_help_block_wrapper do
-        value.validation_msg.present? ? value.validation_msg : ''
-      end
-
-      if value.validation_msg.present?
-        nested_item = nested_item_wrapper(value) do
-          "#{help_block}#{input_label}"
-        end
-      else
-        nested_item = "#{input_label}"
-      end
-
-      "#{input_id ||= '' }#{destroy_input ||= '' }#{nested_item}"
+    if value.new_record?
+      index = value.object_id
     end
+    creator_options = build_creator_options(value, index)
+    input_creator = @builder.text_field(:creator, creator_options)
+
+    index_options = build_index_options(value, index)
+    input_index = @builder.text_field(:index, index_options)
+
+    unless value.new_record?
+      id_options = build_id_options(value.id, index)
+      input_id = @builder.text_field(:id, id_options)
+    end
+
+    if value.destroy_item == true
+      destroy_options = build_destroy_options(value.id, index)
+      destroy_input = @builder.text_field(:_destroy, destroy_options)
+    end
+
+    help_block = nested_item_help_block_wrapper do
+      value.validation_msg.present? ? value.validation_msg : ''
+    end
+
+    if value.validation_msg.present?
+      nested_item = nested_item_wrapper(value) do
+        "#{help_block}#{input_creator}#{input_index}"
+      end
+    else
+      nested_item = "#{input_creator}#{input_index}"
+    end
+
+    "#{input_id ||= '' }#{destroy_input ||= '' }#{nested_item}"
   end
 
   def nested_item_wrapper(value)
@@ -101,13 +101,25 @@ class MultiValueOrderedInput < MultiValueInput
     options
   end
 
-  def build_label_options(value, index)
-    label_value = value.creator.first
-    options = build_field_options(label_value, index)
+  def build_creator_options(value, index)
+    creator_value = value.creator.first
+    options = build_field_options(creator_value, index)
     options[:name] = nested_field_name(:creator.to_s, index)
     options[:id] = nested_field_id(:creator.to_s, index)
     options[:placeholder] = 'Label'
-    options[:readonly] = 'readonly' if value.validation_msg.present? || label_value.present?
+    options[:readonly] = 'readonly' if value.validation_msg.present? || creator_value.present?
+    options
+  end
+
+  def build_index_options(value, index)
+    index_value = value.index.first
+    options = build_field_options(index_value, index)
+    options[:name] = nested_field_name(:index.to_s, index)
+    options[:id] = nested_field_id(:index.to_s, index)
+    options[:placeholder] = 'Index'
+    # uncomment below to make a hidden input
+    # options[:type] = ['hidden']
+    options[:readonly] = 'readonly' if value.validation_msg.present? || index_value.present?
     options
   end
 
@@ -117,5 +129,12 @@ class MultiValueOrderedInput < MultiValueInput
 
   def nested_field_id(property, index)
     "#{object_name}_#{attribute_name}_attributes_#{index.to_s}_#{property}"
+  end
+
+  def collection
+    @collection ||= begin
+      val = object[attribute_name]
+      val.reject { |value| value.to_s.strip.blank? }.sort_by { |h| h[:index].first.to_s }.reverse!
+    end
   end
 end
