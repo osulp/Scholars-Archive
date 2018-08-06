9f0aa9bc53ec933e02cf45fd894958ed7984a251
straleyb <straleyb@onid.oregonstate.edu>
2018-08-06 14:42:42 -0700

    Add observer for running JS

--- app/assets/javascripts/ordered_creator.js
+++ app/assets/javascripts/ordered_creator.js
@@ -7,6 +7,35 @@ function orderedDragAndDrop(selector) {
     });
   });
 }
+
+function reindexNestedOrderedField() {
+  hidden_index_selectors = $($('.ordered-field-container')).find(".index");
+  hidden_index_selectors.each(function (index) {
+    hidden_index_selectors[index].value = index;
+  });
+}
+
+
 Blacklight.onLoad(function() {
+  var mycallback = function(mutationsList) {
+    alert("buggaboo")
+    for(var mutation of mutationsList) {
+      if (mutation.type == 'childList') {
+        console.log('A child node has been added or removed.');
+      }
+    }
+    hidden_index_selectors = $($('.ordered-field-container')).find(".index");
+    hidden_index_selectors.each(function (index) {
+      hidden_index_selectors[index].value = index;
+    });
+  };
+
   orderedDragAndDrop($('.ordered-field-container'));
+
+  var targetNode = document.getElementById('ordered-creator');
+
+  var config = { childList: true, lastChild: true };
+
+  var observer = new MutationObserver(mycallback);
+  observer.observe(targetNode, config);
 });
