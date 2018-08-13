3a03deb0a1fb424e68f7776f7c6394b618be429e
Brandon R. Straley <brandon.straley@oregonstate.edu>
2018-08-13 09:14:22 -0700

    Feature/observer (#1571)
    * Add observer for running JS
    
    * observer working
    
    * WIP for reordering
    
    * Fix some random code issues
    
    * Updates PR

--- app/assets/javascripts/ordered_creator.js
+++ app/assets/javascripts/ordered_creator.js
@@ -1,12 +1,25 @@
 function orderedDragAndDrop(selector) {
-  selector.nestable({maxDepth: 0, rootClass: 'ordered-field-container'});
-  selector.on('change', function(event) {
-    hidden_index_selectors = $(this).find(".index");
-    hidden_index_selectors.each(function( index ) {
-      hidden_index_selectors[index].value = index;
-    });
+  selector.nestable({ maxDepth: 0, rootClass: 'ordered-field-container' });
+  selector.on('change', function (event) {
+    reindexNestedOrderedField();
   });
 }
-Blacklight.onLoad(function() {
+
+function reindexNestedOrderedField(mutationsList) {
+  hidden_index_selectors = $(".ordered-field-container .index");
+  hidden_index_selectors.each(function (index) {
+    hidden_index_selectors[index].value = index;
+  });
+}
+
+Blacklight.onLoad(function () {
   orderedDragAndDrop($('.ordered-field-container'));
+  var nodes = document.querySelectorAll('.dd-list');
+  if (nodes !== null) {
+    var config = { childList: true };
+    var observer = new MutationObserver(reindexNestedOrderedField);
+    nodes.forEach(function (node) {
+      observer.observe(node, config);
+    });
+  }
 });
