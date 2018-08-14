f86bf673a53bc67177dad7e8f844021b907cac61
Josh Gum <revgum@gmail.com>
2018-08-14 08:33:01 -0700

    increases DataDog tracing (#1582)

--- config/initializers/datadog-tracer.rb
+++ config/initializers/datadog-tracer.rb
@@ -3,5 +3,8 @@
 if %w[production staging].include? Rails.env
   Datadog.configure do |c|
     c.use :rails, service_name: "scholars-archive-#{Rails.env}"
+    c.use :http, service_name: "scholars-archive-#{Rails.env}-http"
+    c.use :sidekiq, service_name: "scholars-archive-#{Rails.env}-sidekiq"
+    c.use :redis, service_name: "scholars-archive-#{Rails.env}-redis"
   end
 end
