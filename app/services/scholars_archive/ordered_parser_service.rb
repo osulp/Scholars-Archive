2afd63a988507564bbfd20a1b6fea758e0311019
straleyb <straleyb@onid.oregonstate.edu>
2018-08-14 10:52:08 -0700

    remove unneeded code

--- app/services/scholars_archive/ordered_parser_service.rb
+++ app/services/scholars_archive/ordered_parser_service.rb
@@ -2,11 +2,7 @@ module ScholarsArchive
   class OrderedParserService
     def self.parse(labels)
       labels ||= []
-      parsed_labels = []
-      puts labels
       sorted_labels = labels.map{ |label| build_array(label)}.sort_by{ |array| array[1] }.map{ |array| array[0] }
-      puts sorted_labels
-      sorted_labels
     end
 
     private
