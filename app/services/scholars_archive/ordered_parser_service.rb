98873816574f08bfa2df23feafb41428b7128785
straleyb <straleyb@onid.oregonstate.edu>
2018-08-14 10:26:18 -0700

    Update code to fix small bug

--- app/services/scholars_archive/ordered_parser_service.rb
+++ app/services/scholars_archive/ordered_parser_service.rb
@@ -3,13 +3,16 @@ module ScholarsArchive
     def self.parse(labels)
       labels ||= []
       parsed_labels = []
+      puts labels
       sorted_labels = labels.map{ |label| build_array(label)}.sort_by{ |array| array[1] }.map{ |array| array[0] }
+      puts sorted_labels
+      sorted_labels
     end
 
     private
 
-    def build_array(label)
-      [label.split("$").first, label.split("&").second]
+    def self.build_array(label)
+      [label.split("$").first, label.split("$").second]
     end
   end
 end
