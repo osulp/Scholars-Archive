2ab840296e0bd8b85323679629e19b1cd78dfe8c
Brandon R. Straley <brandon.straley@oregonstate.edu>
2018-08-21 09:59:18 -0700

    Adds ordered creator to pers and remove dup attribute from show (#1595)
    * Adds ordered creator to pers and remove dup attribute from show
    
    * Delete dspace_creator_order.csv

--- app/forms/hyrax/purchased_e_resource_form.rb
+++ app/forms/hyrax/purchased_e_resource_form.rb
@@ -7,11 +7,13 @@ module Hyrax
     include ScholarsArchive::ArticleWorkFormBehavior
 
     self.required_fields = [:title]
+    self.terms -= [:creator]
+
 
     self.model_class = ::PurchasedEResource
 
     def primary_terms
-      [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :bibliographic_citation, :is_referenced_by, :has_journal, :has_volume, :has_number, :conference_location, :conference_name, :conference_section, :editor, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :bibliographic_citation, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | super
+      [:title, :alt_title, :nested_ordered_creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :bibliographic_citation, :is_referenced_by, :has_journal, :has_volume, :has_number, :conference_location, :conference_name, :conference_section, :editor, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :bibliographic_citation, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | super
     end
   end
 end
