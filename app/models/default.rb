afb3213cb1218efe1fb9b575c1cab42094482c7f
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2018-08-27 16:13:06 -0700

    updated models to use nested ordered title for works

--- app/models/default.rb
+++ app/models/default.rb
@@ -12,11 +12,20 @@ class Default < ActiveFedora::Base
 
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
-  validates :title, presence: { message: 'Your work must have a title.' }
 
   self.validates_with ScholarsArchive::Validators::OtherOptionDegreeValidator
   self.validates_with ScholarsArchive::Validators::OtherAffiliationValidator
   self.validates_with ScholarsArchive::Validators::NestedRelatedItemsValidator
+
+  def to_s
+    if title.present?
+      title
+    elsif nested_ordered_title.first.title.present?
+      nested_ordered_title.select { |i| i.title.present? }.map{|i| (i.instance_of? NestedOrderedTitle) ? [i.title.first, i.index.first] : i }.select(&:present?).sort_by{ |array| array[1] }.map{ |array| array[0] }.first
+    else
+      'No Title'
+    end
+  end
 
   private
   def set_defaults
