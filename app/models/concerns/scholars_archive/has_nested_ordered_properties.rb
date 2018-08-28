0352197edbe1597d85260447a1d2592947247853
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2018-08-28 12:15:41 -0700

    implemented ordered_titles in HasNestedOrderedProperties

--- app/models/concerns/scholars_archive/has_nested_ordered_properties.rb
+++ app/models/concerns/scholars_archive/has_nested_ordered_properties.rb
@@ -7,10 +7,46 @@ module ScholarsArchive
         if title.present?
           title
         elsif nested_ordered_title.first.title.present?
-          nested_ordered_title.select { |i| i.title.present? }.map{|i| (i.instance_of? NestedOrderedTitle) ? [i.title.first, i.index.first] : i }.select(&:present?).sort_by{ |array| array[1] }.map{ |array| array[0] }.first
+          ordered_titles.first
         else
           'No Title'
         end
+      end
+
+      # Returns an array of only titles given an array of values that also
+      # include indexes in the form [[title, index], ...].
+      #
+      # ==== Example
+      #
+      # input: [["My Title","0"], ["Another Tilte", "1"]]
+      # output: [["My Title", "Another Title"]]
+      def ordered_titles
+        sort_titles_by_index.map{ |titles| titles.first }
+      end
+
+      # Returns a sorted array (by index value) of nested titles given an array with two
+      # values per element in the form [[title, index],...].
+      #
+      # ==== Example
+      #
+      # input: [["Another Title", "1"], ["My Title", "0"]]
+      # output: [["My Title", "0"], ["Another Title", "1"]]
+      def sort_titles_by_index
+        validate_titles.sort_by{ |titles| titles.second }
+      end
+
+      # Returns an array of items in the form [[title, index], ...] given an
+      # array of items of type NestedOrderedTitle, excluding empty items, and invalid items
+      # if any (nested items with no index or no title values).
+      #
+      # ==== Example
+      #
+      # input: [#<NestedOrderedTitle: ...>, #<NestedOrderedTitle: ...>, ""]
+      # output: [["Another Title", "1"], ["My Title", "0"]]
+      def validate_titles
+        nested_ordered_title.select { |i| i.title.present? && i.index.present? }
+          .map{|i| (i.instance_of? NestedOrderedTitle) ? [i.title.first, i.index.first] : [i] }
+          .select(&:present?)
       end
     end
   end
