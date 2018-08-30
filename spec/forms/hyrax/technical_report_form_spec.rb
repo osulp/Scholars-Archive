49ef87e0338112552b0b8d454bd0a652067f656b
straleyb <straleyb@onid.oregonstate.edu>
2018-08-30 10:36:08 -0700

    fixed tests

--- spec/forms/hyrax/technical_report_form_spec.rb
+++ spec/forms/hyrax/technical_report_form_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Hyrax::TechnicalReportForm do
   end
 
   it "responds to terms with the proper list of terms" do
-    expect(described_class.terms).to include *[:doi, :alt_title, :abstract, :license, :based_near, :resource_type, :date_available, :date_copyright, :date_issued, :date_collected, :date_reviewed, :date_valid, :date_accepted, :replaces, :hydrologic_unit_code, :funding_body, :funding_statement, :in_series, :tableofcontents, :bibliographic_citation, :peerreviewed, :additional_information, :digitization_spec, :file_extent, :file_format, :dspace_community, :dspace_collection]
+    expect(described_class.terms).to include *[:doi, :alt_title, :nested_ordered_abstract, :license, :based_near, :resource_type, :date_available, :date_copyright, :date_issued, :date_collected, :date_reviewed, :date_valid, :date_accepted, :replaces, :hydrologic_unit_code, :funding_body, :funding_statement, :in_series, :tableofcontents, :bibliographic_citation, :peerreviewed, :additional_information, :digitization_spec, :file_extent, :file_format, :dspace_community, :dspace_collection]
   end
 
   it "has the proper required fields" do
@@ -22,7 +22,7 @@ RSpec.describe Hyrax::TechnicalReportForm do
   end
 
   it "has the proper primary terms" do
-    expect(new_form.primary_terms).to include *[:doi, :alt_title, :abstract, :subject, :license]
+    expect(new_form.primary_terms).to include *[:doi, :alt_title, :nested_ordered_abstract, :subject, :license]
   end
 
   it "has the proper secondary terms" do
