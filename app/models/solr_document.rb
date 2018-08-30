17c060ec9b15b6ad4ee2a9a5552efa864d9777c3
straleyb <straleyb@onid.oregonstate.edu>
2018-08-30 10:01:50 -0700

    Fixes rendering for description and abstract

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -91,6 +91,10 @@ class SolrDocument
     self[Solrizer.solr_name('title', :stored_searchable)] || nested_ordered_title_label || []
   end
 
+  def description
+    ScholarsArchive::OrderedParserService.parse(self[Solrizer.solr_name('nested_ordered_description_label', :stored_searchable)] || [])
+  end
+
   def nested_ordered_abstract_label
     ScholarsArchive::OrderedParserService.parse(self[Solrizer.solr_name('nested_ordered_abstract_label', :symbol)])
   end
