c9672d4939f04a5485e4f98cf7c4b5ba95853c02
Josh Gum <revgum@gmail.com>
2018-09-06 10:41:00 -0700

    uses CSVs for abstract and contributor migration

--- config/initializers/work_migrator.rb
+++ config/initializers/work_migrator.rb
@@ -1,4 +1,6 @@
 ::Migrator = ScholarsArchive::MigrateOrderedMetadataService.new(
   creator_csv_path: ENV.fetch('CREATOR_CSV_PATH'),
-  title_csv_path: ENV.fetch('TITLE_CSV_PATH')
+  title_csv_path: ENV.fetch('TITLE_CSV_PATH'),
+  contributor_csv_path: ENV.fetch('CONTRIBUTOR_CSV_PATH'),
+  abstract_csv_path: ENV.fetch('ABSTRACT_CSV_PATH')
 )
