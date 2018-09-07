c1dc7616f66312fcc5854907df03540095d8b8a6
Josh Gum <revgum@gmail.com>
2018-09-07 08:10:57 -0700

    sets default env values for use with test environment

--- config/initializers/work_migrator.rb
+++ config/initializers/work_migrator.rb
@@ -1,6 +1,6 @@
 ::Migrator = ScholarsArchive::MigrateOrderedMetadataService.new(
-  creator_csv_path: ENV.fetch('CREATOR_CSV_PATH'),
-  title_csv_path: ENV.fetch('TITLE_CSV_PATH'),
-  contributor_csv_path: ENV.fetch('CONTRIBUTOR_CSV_PATH'),
-  abstract_csv_path: ENV.fetch('ABSTRACT_CSV_PATH')
+  creator_csv_path: ENV.fetch('CREATOR_CSV_PATH', 'tmp/creator_migration.csv'),
+  title_csv_path: ENV.fetch('TITLE_CSV_PATH', 'tmp/title_migration.csv'),
+  contributor_csv_path: ENV.fetch('CONTRIBUTOR_CSV_PATH', 'tmp/contributor_migration.csv'),
+  abstract_csv_path: ENV.fetch('ABSTRACT_CSV_PATH', 'tmp/abstract_migration.csv')
 )
