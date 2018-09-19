2894cb1cc586a56aced473790ea27b2a96d6a491
Josh Gum <revgum@gmail.com>
2018-09-19 10:51:16 -0700

    fixes Sidekiq administration functions (#1687)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,10 @@
+require 'sidekiq/web'
+Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+Sidekiq::Web.set :sessions,       Rails.application.config.session_options
+Sidekiq::Web.class_eval do
+  use Rack::Protection, origin_whitelist: ['https://ir.library.oregonstate.edu'] # resolve Rack Protection HttpOrigin
+end
+
 config = YAML.safe_load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 
 redis_conn = { url: "redis://#{config[:host]}:#{config[:port]}/" }
