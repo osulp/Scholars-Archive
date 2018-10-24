10c38e0130ab83141a3f4118c7ae715689fa7c7e
luisgreg99 <gregorio.luisramirez@oregonstate.edu>
2018-10-24 09:29:42 -0700

    update purchase e resource form (#1724)

--- app/forms/hyrax/purchased_e_resource_form.rb
+++ app/forms/hyrax/purchased_e_resource_form.rb
@@ -8,12 +8,12 @@ module Hyrax
 
     self.required_fields = [:nested_ordered_title]
     self.terms -= [:creator, :title]
-
+    self.terms -= [:nested_ordered_creator, :nested_ordered_title]
 
     self.model_class = ::PurchasedEResource
 
     def primary_terms
-      [:nested_ordered_title, :alt_title, :nested_ordered_creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :bibliographic_citation, :is_referenced_by, :has_journal, :has_volume, :has_number, :conference_location, :conference_name, :conference_section, :editor, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | [:title, :alt_title, :creator, :contributor, :abstract, :license, :resource_type, :doi, :identifier, :dates_section, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :bibliographic_citation, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | super
+      [:nested_ordered_title, :alt_title, :nested_ordered_creator, :nested_ordered_contributor, :nested_ordered_abstract, :license, :resource_type, :doi, :identifier, :dates_section, :bibliographic_citation, :is_referenced_by, :has_journal, :has_volume, :has_number, :conference_location, :conference_name, :conference_section, :editor, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | [:nested_ordered_title, :alt_title, :nested_ordered_creator, :nested_ordered_contributor, :nested_ordered_abstract, :license, :resource_type, :doi, :identifier, :dates_section, :degree_level, :degree_name, :degree_field, :degree_grantors, :graduation_year, :bibliographic_citation, :academic_affiliation, :other_affiliation, :in_series, :subject, :tableofcontents, :rights_statement] | super
     end
   end
 end
