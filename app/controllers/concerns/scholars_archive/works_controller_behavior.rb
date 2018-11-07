69a8c402fb98c9d13c07e99fde827a3dc49e88d1
Josh Gum <revgum@gmail.com>
2018-11-07 12:15:21 -0800

    redirects a work to its proper route

--- app/controllers/concerns/scholars_archive/works_controller_behavior.rb
+++ app/controllers/concerns/scholars_archive/works_controller_behavior.rb
@@ -3,7 +3,15 @@ module ScholarsArchive
     extend ActiveSupport::Concern
     include Hyrax::WorksControllerBehavior
     included do
+      before_action :redirect_mismatched_work
       before_action :migrate_work_in_place, only: [:edit, :show]
+
+      def redirect_mismatched_work
+        curation_concern = ActiveFedora::Base.find(params[:id])
+        if curation_concern.class != _curation_concern_type
+          redirect_to main_app.polymorphic_path(curation_concern) and return
+        end
+      end
 
       def migrate_work_in_place
         work = ActiveFedora::Base.find(params[:id])
