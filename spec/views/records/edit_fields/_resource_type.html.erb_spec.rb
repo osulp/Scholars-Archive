ce3eb3f1b5da78780c715cbb1b9adad9aded4349
straleyb <straleyb@onid.oregonstate.edu>
2019-04-23 13:05:52 -0700

    rubocop stuff

--- spec/views/records/edit_fields/_resource_type.html.erb_spec.rb
+++ spec/views/records/edit_fields/_resource_type.html.erb_spec.rb
@@ -6,15 +6,13 @@ RSpec.describe 'records/edit_fields/_resource_type.html.erb', type: :view do
   let(:ability) { double(current_user: current_user) }
   let(:current_user) { User.new(email: 'test@example.com', guest: false) }
 
-  
-
-  context "when the work is a graduate thesis" do
+  context 'when the work is a graduate thesis' do
     let(:attributes) { { title: ['test'], creator: ['Blah'], rights_statement: ['blah.blah'], resource_type: [''], degree_name: ['test'] } }
-    let(:work) {
+    let(:work) do
       GraduateThesisOrDissertation.new do |work|
         work.attributes = attributes
       end
-    }
+    end
     let(:f) do
       Hyrax::GraduateThesisOrDissertationForm.new(work, ability, controller)
     end
@@ -23,41 +21,42 @@ RSpec.describe 'records/edit_fields/_resource_type.html.erb', type: :view do
       allow(view).to receive(:signed_in?).and_return(true)
       allow(view).to receive(:current_user).and_return(current_user)
       allow(view).to receive(:can?).and_return(true)
-      allow(Hyrax::ResourceTypesService).to receive(:select_options).and_return([["Honors College Thesis"], ["Dissertation"], ["Masters Thesis"]])
+      allow(Hyrax::ResourceTypesService).to receive(:select_options).and_return([['Honors College Thesis'], ['Dissertation'], ['Masters Thesis']])
       assign(:f, f)
     end
 
     it 'displays the proper select options' do
       render
 
-      expect(rendered).to have_content("Dissertation")
-      expect(rendered).to_not have_content("Honors College Thesis")
+      expect(rendered).to have_content('Dissertation')
+      expect(rendered).not_to have_content('Honors College Thesis')
     end
   end
 
-  context "when the work is a Honors College Thesis" do
+  context 'when the work is a Honors College Thesis' do
     let(:attributes) { { title: ['test'], creator: ['Blah'], rights_statement: ['blah.blah'], resource_type: [''], degree_name: ['test'] } }
-    let(:work) {
+    let(:work) do
       HonorsCollegeThesis.new do |work|
         work.attributes = attributes
       end
-    }
+    end
     let(:f) do
       Hyrax::HonorsCollegeThesisForm.new(work, ability, controller)
     end 
+
     before do
       allow(view).to receive(:signed_in?).and_return(true)
       allow(view).to receive(:current_user).and_return(current_user)
       allow(view).to receive(:can?).and_return(true)
-      allow(Hyrax::ResourceTypesService).to receive(:select_options).and_return([["Honors College Thesis"], ["Dissertation"], ["Masters Thesis"]])
+      allow(Hyrax::ResourceTypesService).to receive(:select_options).and_return([['Honors College Thesis'], ['Dissertation'], ['Masters Thesis']])
       assign(:f, f)
     end
 
     it 'displays the proper select options' do
       render
 
-      expect(rendered).to have_content("Honors College Thesis")
-      expect(rendered).to_not have_content("Dissertation")
+      expect(rendered).to have_content('Honors College Thesis')
+      expect(rendered).not_to have_content('Dissertation')
     end
   end
 end
