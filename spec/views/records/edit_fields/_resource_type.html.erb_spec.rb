262590232b6177da7ea9f70a59d27e1617002576
Josh Gum <revgum@gmail.com>
2019-04-23 14:15:43 -0700

    WIP test

--- spec/views/records/edit_fields/_resource_type.html.erb_spec.rb
+++ spec/views/records/edit_fields/_resource_type.html.erb_spec.rb
@@ -1,73 +1,22 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-require 'rails_helper'
 RSpec.describe 'records/edit_fields/_resource_type.html.erb', type: :view do
-  let(:ability) { instance_double("Current User") }
-  let(:current_user) { User.new(email: 'test@example.com', guest: false) }
+  let(:work) { GraduateThesisOrDissertation.new }
+  let(:form) { Hyrax::GraduateThesisOrDissertationForm.new(work, nil, controller) }
   let(:form_template) do
     %(
       <%= simple_form_for [main_app, @form] do |f| %>
-        <%= render "records/edit_fields/resource_type", f: f, key: 'resource_type' %>
+        <%= render "records/edit_fields/resource_type", f: f, key: 'based_near' %>
       <% end %>
     )
   end
 
-  context 'when the work is a graduate thesis' do
-    let(:attributes) { { title: ['test'], creator: ['Blah'], rights_statement: ['blah.blah'], resource_type: [''], degree_name: ['test'] } }
-    let(:work) do
-      GraduateThesisOrDissertation.new do |work|
-        work.attributes = attributes
-      end
-    end
-    let(:f) do
-      Hyrax::GraduateThesisOrDissertationForm.new(work, ability, controller)
-    end
-
-    before do
-      allow(view).to receive(:signed_in?).and_return(true)
-      allow(view).to receive(:current_user).and_return(current_user)
-      allow(view).to receive(:can?).and_return(true)
-      allow(Hyrax::ResourceTypesService).to receive(:select_options).and_return([['Honors College Thesis'], ['Dissertation'], ['Masters Thesis']])
-      assign(:form, f)
-      render inline: form_template
-    end
-
-    it 'displays the proper select options' do
-      expect(rendered).to have_content('Dissertation')
-    end
-
-    it 'doesnt diplay the rest' do
-      expect(rendered).not_to have_content('Honors College Thesis')
-    end
+  before do
+    assign(:form, form)
+    render inline: form_template
   end
 
-  context 'when the work is a Honors College Thesis' do
-    let(:attributes) { { title: ['test'], creator: ['Blah'], rights_statement: ['blah.blah'], resource_type: [''], degree_name: ['test'] } }
-    let(:work) do
-      HonorsCollegeThesis.new do |work|
-        work.attributes = attributes
-      end
-    end
-    let(:f) do
-      Hyrax::HonorsCollegeThesisForm.new(work, ability, controller)
-    end
-
-    before do
-      allow(view).to receive(:signed_in?).and_return(true)
-      allow(view).to receive(:current_user).and_return(current_user)
-      allow(view).to receive(:can?).and_return(true)
-      allow(Hyrax::ResourceTypesService).to receive(:select_options).and_return([['Honors College Thesis'], ['Dissertation'], ['Masters Thesis']])
-      assign(:form, f)
-      render inline: form_template
-    end
-
-    it 'displays the proper select options' do
-      expect(rendered).to have_content('Honors College Thesis')
-    end
-
-    it 'doesnt display the improper ones' do
-      expect(rendered).not_to have_content('Dissertation')
-    end
+  it 'has url for autocomplete service' do
+    expect(rendered).to have_selector('input[data-autocomplete-url="/authorities/search/geonames"][data-autocomplete="based_near"]')
   end
 end
