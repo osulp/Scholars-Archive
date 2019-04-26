1219a548a8ad6a7fd039ea3235b52846368adae7
luisgreg99 <gregorio.luisramirez@oregonstate.edu>
2019-04-26 07:06:37 -0700

    update date modified format in admin workflows index view (#1844)

---
+++ spec/views/hyrax/admin/workflows/index.html.erb_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe 'hyrax/admin/workflows/index.html.erb', type: :view do
+  let(:doc) do
+    SolrDocument.new(id: 'abc123',
+                     has_model_ssim: ['Default'],
+                     date_modified_dtsi: modified_date,
+                     title_tesim: ['Submission Title 999'])
+  end
+
+  let(:modified_date) { 'Wed Apr 24 21:22:44 2019' }
+
+  before do
+    assign(:published_list, [doc])
+    assign(:status_list, [doc])
+    render
+  end
+
+  context 'when user has submissions to review' do
+    it 'displays submission date in the format YYYY-MM-DD HH:MM:SS' do
+      expect(rendered).to include('2019-04-24 21:22:44')
+    end
+  end
+end
