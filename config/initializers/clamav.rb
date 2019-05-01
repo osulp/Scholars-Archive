6e51e38dc0f62549a6ebb0d0275fa9fa8957b5e4
Josh Gum <revgum@gmail.com>
2019-05-01 11:23:03 -0700

    disables clamav until it can be verified to work on production

--- config/initializers/clamav.rb
+++ config/initializers/clamav.rb
@@ -1,3 +1,3 @@
-if Rails.env.staging? || Rails.env.production?
-  ClamAV.instance.loaddb if defined? ClamAV
-end
+#if Rails.env.staging? || Rails.env.production?
+#  ClamAV.instance.loaddb if defined? ClamAV
+#end
