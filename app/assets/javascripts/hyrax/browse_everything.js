4f4b9810903572ded6d58d8856993c88fdcacf90
straleyb <straleyb@onid.oregonstate.edu>
2019-05-03 11:28:10 -0700

    revert browse everything

--- app/assets/javascripts/hyrax/browse_everything.js
+++ app/assets/javascripts/hyrax/browse_everything.js
@@ -2,16 +2,14 @@
 OVERRIDE: This files overrides browse_everything.js so that browseEverything 
 can be loaded properly. It seems to be a known issue in hyrax. See issue 169:
 https://github.com/samvera/browse-everything/issues/169
-
 Base file:
 https://github.com/samvera/hyrax/blob/v2.3.0/app/assets/javascripts/hyrax/browse_everything.js
-
 */
 
 //= require jquery.treetable
 //= require browse_everything/behavior
 
-Blacklight.onLoad(function() {
+$(document).on('ready turbolinks:load', function() {
   if ($('#browse-btn').length > 0) {
     $('#browse-btn').browseEverything()
       .done(function(data) {
