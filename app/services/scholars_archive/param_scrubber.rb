44060ccd71f965dab9f1f6bde39453241d982cae
straleyb <straleyb@onid.oregonstate.edu>
2019-05-21 09:38:18 -0700

    fix again

--- app/services/scholars_archive/param_scrubber.rb
+++ app/services/scholars_archive/param_scrubber.rb
@@ -30,13 +30,9 @@ module ScholarsArchive
 
     # Map and strip the values on the array
     def self.mapped_values(value)
-      new_values = []
-      if value.first.is_a? ActionController::Parameters
-        new_values = value.map { |val| extract_hash_values(val) }
-      else
-        new_values = value.map { |v| v.strip unless v.nil? || v.frozen? }
-      end
-      new_values
+      return value.map { |val| extract_hash_values(val) } if value.first.is_a? ActionController::Parameters
+
+      value.map { |v| v.strip unless v.nil? || v.frozen? }
     end
 
     # Three different ways to set properties
