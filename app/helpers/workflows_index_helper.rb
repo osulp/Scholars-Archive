df93fdcabd392bd9e6ba63f2346829d4af6d130c
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2019-06-07 16:56:08 -0700

    update style for rubocop

--- app/helpers/workflows_index_helper.rb
+++ app/helpers/workflows_index_helper.rb
@@ -3,7 +3,7 @@
 # Workflows Index view helper
 module WorkflowsIndexHelper
   def local_time_from_utc(value)
-    utc_t = Time.find_zone("UTC").parse(value)
-    utc_t.in_time_zone(Time.zone).strftime("%Y-%m-%d %H:%M:%S")
+    utc_t = Time.find_zone('UTC').parse(value)
+    utc_t.in_time_zone(Time.zone).strftime('%Y-%m-%d %H:%M:%S')
   end
 end
