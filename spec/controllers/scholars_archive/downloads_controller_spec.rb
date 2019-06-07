3bdb2fa2295d6e2dffee578d95b58c5d2bda7182
luisgreg99 <gregorio.luisramirez@oregonstate.edu>
2019-06-07 12:58:09 -0700

    add rescue err to track_download (#1885)

--- spec/controllers/scholars_archive/downloads_controller_spec.rb
+++ spec/controllers/scholars_archive/downloads_controller_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe ScholarsArchive::DownloadsController, type: :controller do
     expect(controller).to respond_to(:track_download)
     expect(controller.track_download).to be_a_kind_of Net::HTTPOK
   end
+
+  context 'when rescuing errors from Staccato' do
+    let(:error) { StandardError }
+
+    before do
+      allow(Staccato).to receive(:tracker).and_raise(error)
+      stub_request(:post, 'http://www.google-analytics.com/collect').to_return(status: 200, body: '', headers: {})
+    end
+
+    it 'responds to track_download' do
+      expect(controller).to respond_to(:track_download)
+    end
+    it 'rescues errors from Staccato' do
+      expect(controller.track_download).to be_nil
+    end
+  end
 end
