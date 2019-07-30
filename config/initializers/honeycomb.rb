3d73ffad3411deb0df169aaf0d3c7877b7a73050
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2019-07-30 10:39:12 -0700

    Added config.notification_events for Honeycomb initializer

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,4 +1,14 @@
 Honeycomb.configure do |config|
   config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', 'fa01b2227f761b5c1f11ae1a680f14da')
   config.dataset = ENV.fetch('HONEYCOMB_DATASET', 'scholars-staging')
+  config.notification_events = %w[
+    sql.active_record
+    render_template.action_view
+    render_partial.action_view
+    render_collection.action_view
+    process_action.action_controller
+    send_file.action_controller
+    send_data.action_controller
+    deliver.action_mailer
+  ].freeze
 end
