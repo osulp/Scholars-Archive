af3fb2eba7484210fa9815f95eb94165187d77b1
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2019-07-30 14:25:30 -0700

    Set HONEYCOMB_DEBUG when running tests

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,6 +1,6 @@
 Honeycomb.configure do |config|
-  config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', '')
-  config.dataset = ENV.fetch('HONEYCOMB_DATASET', '')
+  config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', 'fa01b2227f761b5c1f11ae1a680f14da')
+  config.dataset = ENV.fetch('HONEYCOMB_DATASET', 'scholars-staging')
   config.notification_events = %w[
     sql.active_record
     render_template.action_view
@@ -11,4 +11,8 @@ Honeycomb.configure do |config|
     send_data.action_controller
     deliver.action_mailer
   ].freeze
+
+  if %w[test].include? Rails.env
+    ENV[HONEYCOMB_DEBUG] = true
+  end
 end
