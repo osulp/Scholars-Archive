0628f94b33a91f32de4c4863deda35193d79bbba
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2019-07-30 11:34:08 -0700

    Don't set honeycomb defaults

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,6 +1,6 @@
 Honeycomb.configure do |config|
-  config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', 'fa01b2227f761b5c1f11ae1a680f14da')
-  config.dataset = ENV.fetch('HONEYCOMB_DATASET', 'scholars-staging')
+  config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', '')
+  config.dataset = ENV.fetch('HONEYCOMB_DATASET', '')
   config.notification_events = %w[
     sql.active_record
     render_template.action_view
