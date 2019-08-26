a3db5c3408f77602ad928b0bb97455ba7df00228
Ryan Wick <ryan.wick@oregonstate.edu>
2019-08-26 09:54:28 -0700

    Social media (#1913)
    * Add modified social media view from OD2.
    
    * Add magic comment and split up expectations.
    
    * Fix spec.

---
+++ spec/views/hyrax/base/_social_media.html.erb_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe 'hyrax/base/_social_media.html.erb', type: :view do
+  let(:url) { 'http://example.com/' }
+  let(:title) { 'Example' }
+  let(:page) do
+    render partial: 'hyrax/base/social_media', locals: { share_url: url, page_title: title }
+    Capybara::Node::Simple.new(rendered)
+  end
+
+  it 'renders various social media share button' do
+    expect(page).to have_link '', href: 'https://facebook.com/sharer/sharer.php?u=http%3A%2F%2Fexample.com%2F'
+  end
+  it 'renders Twitter share button' do
+    expect(page).to have_link '', href: 'https://twitter.com/intent/tweet/?text=Example&url=http%3A%2F%2Fexample.com%2F'
+  end
+  it 'renders Copy Share Link button' do
+    expect(page).to have_button 'Copy Share Link'
+  end
+end
