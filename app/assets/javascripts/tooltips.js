bd793ab41f12a36d15173f522c18def5d3601ea5
straleyb <straleyb@onid.oregonstate.edu>
2019-09-05 14:01:09 -0700

    verbiage updates

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -1,13 +1,17 @@
 Blacklight.onLoad(function() {
   var search_options = {
     placement: 'top',
-    title: 'Search SA@OSU'
+    title: 'Search for this term in SA@OSU'
   };
   var mailto_options = {
     placement: 'top',
     title: 'Compose an email'
   };
   var more_information = {
+    placement: 'top',
+    title: 'More information about this item'
+  };
+  var external_information = {
     placement: 'top',
     title: 'Visit External Link'
   };
@@ -16,5 +20,5 @@ Blacklight.onLoad(function() {
   $('.metadata > .table a').not('.btn').tooltip(search_options);
   $('li.attribute a').not('.btn').not('[href^="http"]').tooltip(search_options);
   $('li.attribute a.btn').tooltip(more_information);
-  $('li.attribute a[href^="http"]').tooltip(more_information);
+  $('li.attribute a[href^="http"]').tooltip(external_information);
 });
