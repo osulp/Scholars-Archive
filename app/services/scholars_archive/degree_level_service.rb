919b860890779d99e2302f3b729e4e6776bda559
straleyb <straleyb@onid.oregonstate.edu>
2019-09-09 13:21:30 -0700

    single quotes

--- app/services/scholars_archive/degree_level_service.rb
+++ app/services/scholars_archive/degree_level_service.rb
@@ -13,16 +13,16 @@ module ScholarsArchive
 
     def work_type_limited_options(work_type)
       case work_type
-      when "UndergraduateThesisOrProject"
-        select_all_options.sort.select! { |opt| opt.first.include?("Batchelor's") }
-      when "GraduateProject"
-        select_all_options.sort.select! { |opt| opt.first.include?("Master's") }
-      when "GraduateThesisOrDissertation"
-        select_all_options.sort.select! { |opt| opt.first.include?("Doctoral") || opt.first.include?("Master's") }
-      when "HonorsCollegeThesis"
-        select_all_options.sort.select! { |opt| opt.first.include?("Doctoral") || opt.first.include?("Master's") }
-      when "PurchasedEResource"
-        select_all_options.sort.select! { |opt| opt.first.include?("Doctoral") || opt.first.include?("Master's") }
+      when 'UndergraduateThesisOrProject'
+        select_all_options.sort.select! { |opt| opt.first.include?('Batchelor\'s') }
+      when 'GraduateProject'
+        select_all_options.sort.select! { |opt| opt.first.include?('Master\'s') }
+      when 'GraduateThesisOrDissertation'
+        select_all_options.sort.select! { |opt| opt.first.include?('Doctoral') || opt.first.include?('Master\'s') }
+      when 'HonorsCollegeThesis'
+        select_all_options.sort.select! { |opt| opt.first.include?('Doctoral') || opt.first.include?('Master\'s') }
+      when 'PurchasedEResource'
+        select_all_options.sort.select! { |opt| opt.first.include?('Doctoral') || opt.first.include?('Master\'s') }
       else
         select_all_options.sort
       end
