e71771801fd4be6145bf194f98ea9e781f00c836
luisgreg99 <gregorio.luisramirez@oregonstate.edu>
2019-09-27 08:01:25 -0700

    update embargo_releaser to catch error during embergo lifting task (#1966)

--- lib/scholars_archive/embargoes/embargo_releaser.rb
+++ lib/scholars_archive/embargoes/embargo_releaser.rb
@@ -9,25 +9,31 @@ module ScholarsArchive::Embargoes
 
       expired_embargoes = Hyrax::EmbargoService.assets_with_expired_embargoes
       expired_embargoes.each do |embargo|
+        unless ActiveFedora::Base.exists?(embargo.solr_document[:id])
+          logger.warn("Skipping work #{embargo.solr_document[:id]} since it doesn't exist. Couldnt process embargo release for this work.")
+          next
+        end
+
         work = ActiveFedora::Base.find(embargo.solr_document[:id])
+
         next if work.under_embargo?
 
         logger.warn("Processing work id: #{work.id}")
-        begin
-          work.embargo_visibility!
-          work.deactivate_embargo!
-          work.embargo.save!
+
+        work.embargo_visibility!
+        work.deactivate_embargo!
+        work.embargo.save!
+        work.save!(validate: false)
+        if work.file_set?
+          work.visibility = work.to_solr['visibility_after_embargo_ssim']
           work.save!(validate: false)
-          if work.file_set?
-            work.visibility = work.to_solr['visibility_after_embargo_ssim']
-            work.save!(validate: false)
-          elsif !work.file_set?
-            work.copy_visibility_to_files
-          end
-        rescue => e
-          logger.warn("Couldnt process #{work.id}")
-          logger.warn([e.message]+e.backtrace).join("\n")
+        elsif !work.file_set?
+          work.copy_visibility_to_files
         end
+      rescue StandardError => e
+        logger.error("Couldnt process #{work.id}")
+        logger.error([e.message]+e.backtrace).join("\n")
+        return nil
       end
     end
   end
