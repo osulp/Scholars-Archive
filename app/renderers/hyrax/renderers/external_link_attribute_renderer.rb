995078d831a0545365a6f54f3a05bbe857d80b7d
straleyb <straleyb@onid.oregonstate.edu>
2019-11-05 12:48:18 -0800

    make it raw

--- app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
+++ app/renderers/hyrax/renderers/external_link_attribute_renderer.rb
@@ -7,8 +7,8 @@ module Hyrax
       private
 
         def li_value(value)
-          normalized_link = value.include?('http://') ? auto_link(value) : "http://#{auto_link(value)}".html_safe
-          "<a href='#{normalized_link.html_safe}'>#{value}</a>&nbsp;&nbsp;<span class='glyphicon glyphicon-new-window'></span>".html_safe
+          normalized_link = value.include?('http://') ? auto_link(value) : "http://#{auto_link(value)}".raw
+          "<a href='#{normalized_link.raw}'>#{value}</a>&nbsp;&nbsp;<span class='glyphicon glyphicon-new-window'></span>".raw
         end
     end
   end
