2bd370bdf3924625cb93b4242f71607b4c18eec4
straleyb <straleyb@onid.oregonstate.edu>
2019-11-06 13:36:15 -0800

    update tr_content

--- spec/renderers/hyrax/renderers/external_link_attribute_renderer_spec.rb
+++ spec/renderers/hyrax/renderers/external_link_attribute_renderer_spec.rb
@@ -12,23 +12,15 @@ RSpec.describe Hyrax::Renderers::ExternalLinkAttributeRenderer do
 
     let(:tr_content) do
       "<tr><th>Name</th>\n" \
-       "<td><ul class='tabular'>" \
-       '<li class="attribute attribute-name">'\
-       '<a href="http://example.com">'\
-       "http://example.com&nbsp;&nbsp;<span class='glyphicon glyphicon-new-window'></span></a></li>"\
-       '</ul></td></tr>'
+      '<td><ul class="tabular">' \
+      '<li class="attribute attribute-name">'\
+      '<a href="&lt;a%20href=%22http://example.com%22&gt;http://example.com&lt;/a&gt;">'\
+      'http://example.com</a>  <span class="glyphicon glyphicon-new-window"></span></li>'\
+      '</ul></td></tr>'
     end
 
     it do
-      expect(subject).to be_equivalent_to(
-        '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-                <html><body><tr>
-                <th>Name</th>
-                <td><ul class="tabular"><li class="attribute attribute-name">
-                <a href="&lt;a%20href=%22http://example.com%22&gt;http://example.com&lt;/a&gt;">http://example.com</a>  <span class="glyphicon glyphicon-new-window"></span>
-                </li></ul></td>
-                </tr></body></html>'
-      )
+      expect(subject).to be_equivalent_to(expected)
     end
   end
 end
