bbadfcf6e39e39390a4117a7fb4e39efd0d9cd15
straleyb <straleyb@onid.oregonstate.edu>
2020-02-20 13:05:08 -0800

    adds comments

--- app/helpers/oregon_digital/citations_behaviors/formatters/apa_formatter.rb
+++ app/helpers/oregon_digital/citations_behaviors/formatters/apa_formatter.rb
@@ -1,9 +1,12 @@
 module OregonDigital
   module CitationsBehaviors
     module Formatters
+      # OVERRIDES SETUP PUB DATE TO USE DATE ISSUED
       class ApaFormatter < Hyrax::CitationsBehaviors::Formatters::ApaFormatter
         def setup_pub_date(work)
+          # OVERRIDE HERE
           first_date = work.solr_document.date_issued.first if work.solr_document.date_issued
+          # END OVERRIDE
           if first_date.present?
             first_date = CGI.escapeHTML(first_date)
             date_value = first_date.gsub(/[^0-9|n\.d\.]/, "")[0, 4]
