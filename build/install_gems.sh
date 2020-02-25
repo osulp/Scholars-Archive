20a7d3453e89f363646f6bf364b715ea2b9473ae
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2020-02-25 15:08:23 -0800

    build tests

--- build/install_gems.sh
+++ build/install_gems.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+nproc=2
+
 if [ "${RAILS_ENV}" = 'production' ]; then
   echo "Bundle install without development or test gems."
   bundle install --without development test -j $(nproc)
