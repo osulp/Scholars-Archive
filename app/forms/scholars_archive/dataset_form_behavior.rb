da5f351344bbef7d6bd95e726e850a5814a055ef
straleyb <straleyb@onid.oregonstate.edu>
2020-03-02 12:16:12 -0800

    dataset work form change

--- app/forms/scholars_archive/dataset_form_behavior.rb
+++ app/forms/scholars_archive/dataset_form_behavior.rb
@@ -11,16 +11,14 @@ module ScholarsArchive
       # accessor attributes only used to group dates and geo fields and allow proper ordering in this form
       attr_accessor :dates_section
       attr_accessor :geo_section
-
       attr_accessor :other_affiliation_other
 
-      self.terms += %i[nested_related_items date_uploaded date_modified doi other_affiliation academic_affiliation alt_title nested_ordered_abstract license resource_type date_available date_copyright date_issued date_collected date_valid date_reviewed date_accepted replaces nested_geo hydrologic_unit_code funding_body funding_statement in_series tableofcontents bibliographic_citation peerreviewed nested_ordered_additional_information digitization_spec file_extent file_format dspace_community dspace_collection isbn issn embargo_reason]
-
+      self.terms += Default::DATASET_PRIMARY_TERMS
       self.required_fields += %i[resource_type nested_ordered_creator nested_ordered_title]
       self.required_fields -= %i[keyword creator title]
 
       def primary_terms
-        t = %i[nested_ordered_title alt_title nested_ordered_creator academic_affiliation other_affiliation nested_ordered_contributor nested_ordered_abstract license resource_type doi dates_section bibliographic_citation in_series subject rights_statement nested_related_items hydrologic_unit_code geo_section funding_statement publisher peerreviewed language digitization_spec replaces nested_ordered_additional_information] | super
+        t =  | super
         t << [:description] if current_ability.current_user.admin?
         t.flatten
       end
