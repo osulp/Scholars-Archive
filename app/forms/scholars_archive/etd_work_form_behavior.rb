0fc5e662b4a9669c90f926fa69828280abd4971d
straleyb <straleyb@onid.oregonstate.edu>
2020-03-02 12:17:31 -0800

    etd work form change

--- app/forms/scholars_archive/etd_work_form_behavior.rb
+++ app/forms/scholars_archive/etd_work_form_behavior.rb
@@ -8,11 +8,11 @@ module ScholarsArchive
       include ScholarsArchive::DefaultWorkFormBehavior
       attr_accessor :degree_grantors_other
 
-      self.terms += %i[degree_grantors contributor_committeemember graduation_year degree_discipline]
+      self.terms += Etd.etd_properties
       self.required_fields += %i[degree_level degree_name degree_field degree_grantors graduation_year]
 
       def primary_terms
-        %i[nested_ordered_title alt_title nested_ordered_creator nested_ordered_contributor nested_ordered_abstract license resource_type doi dates_section degree_level degree_name degree_field degree_grantors graduation_year contributor_advisor contributor_committeemember bibliographic_citation academic_affiliation other_affiliation in_series subject tableofcontents rights_statement] | super
+        Etd::ETD_PRIMARY_TERMS
       end
 
       def secondary_terms
