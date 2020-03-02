07a83c3047fb90ed4b91f89f440abaad6474e478
straleyb <straleyb@onid.oregonstate.edu>
2020-03-02 12:15:49 -0800

    article work form change

--- app/forms/scholars_archive/article_work_form_behavior.rb
+++ app/forms/scholars_archive/article_work_form_behavior.rb
@@ -5,16 +5,18 @@ module ScholarsArchive
   module ArticleWorkFormBehavior
     extend ActiveSupport::Concern
     included do
-      self.terms += %i[resource_type editor has_volume has_number conference_location conference_name conference_section has_journal is_referenced_by web_of_science_uid]
+      self.terms += Article.article_properties
 
       def primary_terms
-        %i[nested_ordered_title alt_title nested_ordered_creator nested_ordered_contributor nested_ordered_abstract license resource_type doi dates_section bibliographic_citation is_referenced_by has_journal has_volume has_number conference_name conference_section conference_location editor academic_affiliation other_affiliation in_series subject tableofcontents rights_statement] | super - %i[degree_level degree_name degree_field contributor_advisor]
+        Article::ARTICLE_PRIMARY_TERMS | super - %i[degree_level degree_name degree_field contributor_advisor]
       end
 
       def secondary_terms
-        t = super - date_terms - %i[conference_location conference_name conference_section]
-        t << :web_of_science_uid if current_ability.current_user.admin?
-        t
+        current_ability.current_user.admin? ? super - removed_secondary_terms + :web_of_science_uid : super - removed_secondary_terms
+      end
+
+      def removed_secondary_terms
+        date_terms - %i[conference_location conference_name conference_section]
       end
     end
   end
