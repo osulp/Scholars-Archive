b602e3098c3ce5b842ad08a21e0561aaaf428dbf
straleyb <straleyb@onid.oregonstate.edu>
2020-03-04 07:06:14 -0800

    etd primary terms instead

--- app/forms/scholars_archive/etd_work_form_behavior.rb
+++ app/forms/scholars_archive/etd_work_form_behavior.rb
@@ -8,7 +8,7 @@ module ScholarsArchive
       include ScholarsArchive::DefaultWorkFormBehavior
       attr_accessor :degree_grantors_other
 
-      self.terms += model_class::ETD_TERMS
+      self.terms += model_class::ETD_PRIMARY_TERMS
       self.required_fields += %i[degree_level degree_name degree_field degree_grantors graduation_year]
 
       def primary_terms
