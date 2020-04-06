8c78dc24e6f66772156e02c103abd9bbd8d42edd
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2020-04-06 13:00:06 -0700

    whitespace to trigger a build

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -1,3 +1,4 @@
+# Honeycomb initializer
 Honeycomb.configure do |config|
   config.write_key = ENV.fetch('HONEYCOMB_WRITEKEY', 'fa01b2227f761b5c1f11ae1a680f14da')
   config.dataset = ENV.fetch('HONEYCOMB_DATASET', 'scholars-staging')
