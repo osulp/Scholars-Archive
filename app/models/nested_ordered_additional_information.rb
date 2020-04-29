3db990623fe05e6b5d5b00de997e2c60753ddcc8
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2020-04-29 12:19:40 -0700

    Revert "refactor nested objects to inherit from a base resource." (#2089)

--- app/models/nested_ordered_additional_information.rb
+++ app/models/nested_ordered_additional_information.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 # nested ordered additional information object
-class NestedOrderedAdditionalInformation < NestedOrderedResource
+class NestedOrderedAdditionalInformation < ActiveTriples::Resource
   # Usage notes and expectations can be found in the Metadata Application Profile:
   #   https://docs.google.com/spreadsheets/d/1koKjV7bjn7v4r5a3gsowEimljHiAwbwuOgjHe7FEtuw/edit?usp=sharing
 
   property :index, predicate: ::RDF::URI('http://purl.org/ontology/olo/core#index')
   property :additional_information, predicate: ::RDF::Vocab::DC.description
+
+  attr_accessor :destroy_item # true/false
+  attr_accessor :validation_msg # string
 
   def initialize(uri=RDF::Node.new, parent=nil)
     if uri.try(:node?)
@@ -15,5 +18,17 @@ class NestedOrderedAdditionalInformation < NestedOrderedResource
       uri = RDF::URI(uri)
     end
     super
+  end
+
+  def final_parent
+    parent
+  end
+
+  def new_record?
+    id.start_with?('#')
+  end
+
+  def _destroy
+    false
   end
 end
