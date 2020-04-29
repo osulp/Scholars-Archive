96a88903f8d17a4cacbef93543c9df76479951d6
Brandon R. Straley <brandon.straley@oregonstate.edu>
2020-04-29 09:42:41 -0700

    Revert "Feature/refine tombstone"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 # Application controller
 class ApplicationController < ActionController::Base
   helper Openseadragon::OpenseadragonHelper
-  helper Hyrax::Engine.helpers
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   include Hydra::Controller::ControllerBehavior
