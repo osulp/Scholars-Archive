f05212114b791fd8ba7c40fe2bae169617ede503
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2020-05-26 11:35:56 -0700

    remove render_partial.action_view

--- config/initializers/honeycomb.rb
+++ config/initializers/honeycomb.rb
@@ -4,7 +4,6 @@ Honeycomb.configure do |config|
   config.notification_events = %w[
     sql.active_record
     render_template.action_view
-    render_partial.action_view
     render_collection.action_view
     process_action.action_controller
     send_file.action_controller
