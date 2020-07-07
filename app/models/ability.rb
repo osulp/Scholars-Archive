666c97c34b928643d55547c404226215d1550d1d
Greg Luis <gregorio.luisramirez@oregonstate.edu>
2020-07-07 11:56:33 -0700

    Revert "updates to allow admin to edit works (#2095)" (#2104)
    This reverts commit 86163deb8c84c7c2b7e633e73a3d946d63c6cf6e.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,11 +22,11 @@ class Ability
     # end
 
     can %i[edit update], SolrDocument do |solr_doc|
-      AdminSet.where(title: solr_doc.admin_set).first.edit_users.include?(current_user.username) || current_user.admin?
+      AdminSet.where(title: solr_doc.admin_set).first.edit_users.include?(current_user.username)
     end
 
     can %i[edit update], ActiveFedora::Base do |record|
-      record.admin_set.edit_users.include?(current_user.username) || current_user.admin?
+      record.admin_set.edit_users.include?(current_user.username)
     end
   end
 end
