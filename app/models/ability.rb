8e91fc7832b7fed517b89ed826ab050aef0b6ae5
Greg Luis <gregorio.luisramirez@oregonstate.edu>
2020-07-10 11:20:42 -0700

    Revert 2105 revert 2048 feature/allow editing if manager (#2107)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,5 +20,13 @@ class Ability
     # if user_groups.include? 'special_group'
     #   can [:create], ActiveFedora::Base
     # end
+
+    can %i[edit update], SolrDocument do |solr_doc|
+      (AdminSet.where(title: solr_doc.admin_set).first.edit_users.include?(current_user.username) || current_user.admin?) if solr_doc.admin_set.present? && current_user.present?
+    end
+
+    can %i[edit update], ActiveFedora::Base do |record|
+      record.admin_set.edit_users.include?(current_user.username) || current_user.admin?
+    end
   end
 end
