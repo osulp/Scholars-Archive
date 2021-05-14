3520e084967e594d19aa657e807dcd0bea6dee75
Ryan Ordway <rordway@oregonstate.edu>
2021-05-13 18:07:37 -0700

    build script to push images for testing

---
+++ build/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+build_no="$1"
+build_args="--compress"
+
+if [ -z "$BCR_PASS" ]; then
+   echo 'Please set the BCR password in $BCR_PASS'
+   exit 1
+fi
+
+if [ -z "$build_no" ]; then
+   if [ -f ".version" ]; then
+      version=`cat .version`
+      let build_no=($version + 1)
+      echo "Using cached build number: (old: $version, new: $build_no)"
+   else
+      echo "Usage: $0 <build number>"
+      exit 1
+   fi
+fi
+tag1="registry.library.oregonstate.edu/sa_web:osulp-${build_no}"
+
+echo "Building for tag $tag1"
+docker build ${build_args} . -t "$tag1"
+
+echo "Logging into BCR as admin"
+echo $BCR_PASS | docker login --password-stdin registry.library.oregonstate.edu
+
+echo "pushing: $tag1"
+docker push "$tag1"
+echo "$build_no" > .version
