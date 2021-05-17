b260f8da034e2fc389d94c88a097fe6a7b17c42a
Ryan Ordway <rordway@oregonstate.edu>
2021-05-17 13:23:35 -0700

    don't include dev/test gems for production or staging

--- build/install_gems.sh
+++ build/install_gems.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-nproc=2
+nproc=4
 
-if [ "${RAILS_ENV}" = 'production' ]; then
+if [ "${RAILS_ENV}" == 'production' ]; then
   echo "Bundle install without development or test gems."
-  bundle install --without development test -j $(nproc)
+  bundle install --without development test -j $nproc
+elif [ "${RAILS_ENV}" == 'staging' ]; then
+  echo "Bundle install without development or test gems."
+  bundle install --without development test -j $nproc
 else
-  bundle install -j $(nproc)
+  echo "Bundle install with all gems (+development +test)."
+  bundle install -j $nproc
 fi
