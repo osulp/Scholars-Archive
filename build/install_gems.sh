4d5a7817410800aad98a94da1229a8d08a01d16f
Ryan Ordway <rordway@oregonstate.edu>
2021-05-17 16:01:59 -0700

    fixed env problems

--- build/install_gems.sh
+++ build/install_gems.sh
@@ -2,10 +2,10 @@
 
 nproc=4
 
-if [ "${RAILS_ENV}" = 'production' -o "$RAILS_ENV" = 'staging' ]; then
-  echo "Bundle install without development or test gems."
+if [ "${RAILS_ENV}" == 'production' -o "$RAILS_ENV" == 'staging' ]; then
+  echo "Bundle install without development or test gems. ($RAILS_ENV)"
   bundle install --without development test -j $nproc
 else
-  echo "Bundle install with all gems (+development +test)."
+  echo "Bundle install with all gems (+development +test). ($RAILS_ENV)"
   bundle install -j $nproc
 fi
