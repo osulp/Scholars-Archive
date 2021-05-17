64f04ba152cb588a944f73bb09b8cf1f16ecfced
Ryan Ordway <rordway@oregonstate.edu>
2021-05-17 16:02:30 -0700

    pass RAILS_ENV to docker build

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 build_no="$1"
-build_args="--compress"
+build_args="--compress --build-arg RAILS_ENV=$RAILS_ENV"
 
 if [ -z "$BCR_PASS" ]; then
    echo 'Please set the BCR password in $BCR_PASS'
@@ -21,7 +21,7 @@ fi
 tag1="registry.library.oregonstate.edu/sa_web:osulp-${build_no}"
 
 echo "Building for tag $tag1"
-docker build ${build_args} . -t "$tag1"
+RAILS_ENV=$RAILS_ENV docker build ${build_args} . -t "$tag1"
 
 echo "Logging into BCR as admin"
 echo $BCR_PASS | docker login --password-stdin registry.library.oregonstate.edu
