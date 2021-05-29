e63073628a122bfde278666dcf07b6189a31bc07
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-05-28 18:06:01 -0700

    force env for testing

--- build/install_gems.sh
+++ build/install_gems.sh
@@ -2,8 +2,16 @@
 
 if [ "${RAILS_ENV}" == 'production' -o "$RAILS_ENV" == 'staging' ]; then
   echo "Bundle install without development or test gems. ($RAILS_ENV)"
-  bundle install --without development test -j $(nproc)
+  export BUNDLE_CLEAN='true'
+  export BUNDLE_JOBS=8
+  export BUNDLE_IGNORE_MESSAGES='true'
+  export BUNDLE_WITHOUT='development:test'
+  bundle install -j $(nproc)
 else
   echo "Bundle install with all gems (+development +test). ($RAILS_ENV)"
+  export BUNDLE_CLEAN='true'
+  export BUNDLE_JOBS=8
+  export BUNDLE_IGNORE_MESSAGES='false'
+  export BUNDLE_WITHOUT=''
   bundle install -j $(nproc)
 fi
