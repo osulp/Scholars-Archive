6e9f527035ac3951ce1d687d541d0deafcc51bde
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-05-28 18:33:46 -0700

    BUNDLE_CLEAN doesn't seem to let you force

--- build/install_gems.sh
+++ build/install_gems.sh
@@ -2,14 +2,12 @@
 
 if [ "${RAILS_ENV}" == 'production' -o "$RAILS_ENV" == 'staging' ]; then
   echo "Bundle install without development or test gems. ($RAILS_ENV)"
-  export BUNDLE_CLEAN='true'
   export BUNDLE_JOBS=8
   export BUNDLE_IGNORE_MESSAGES='true'
   export BUNDLE_WITHOUT='development:test'
   bundle install -j $(nproc)
 else
   echo "Bundle install with all gems (+development +test). ($RAILS_ENV)"
-  export BUNDLE_CLEAN='true'
   export BUNDLE_JOBS=8
   export BUNDLE_IGNORE_MESSAGES='false'
   export BUNDLE_WITHOUT=''
