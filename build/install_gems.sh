3071da0d2fcafde9735a1e965d0e884e53e23acd
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-05-28 18:00:17 -0700

    use the nproc binary rather than a static number

--- build/install_gems.sh
+++ build/install_gems.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-nproc=4
-
 if [ "${RAILS_ENV}" == 'production' -o "$RAILS_ENV" == 'staging' ]; then
   echo "Bundle install without development or test gems. ($RAILS_ENV)"
-  bundle install --without development test -j $nproc
+  bundle install --without development test -j $(nproc)
 else
   echo "Bundle install with all gems (+development +test). ($RAILS_ENV)"
-  bundle install -j $nproc
+  bundle install -j $(nproc)
 fi
