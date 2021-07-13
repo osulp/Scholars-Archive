c1ebb854f4ddb61d6949b6d6e18084a06528c7ae
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-07-13 10:21:50 -0700

    fix rubocop?

--- spec/presenters/hyrax/honors_college_thesis_presenter_spec.rb
+++ spec/presenters/hyrax/honors_college_thesis_presenter_spec.rb
@@ -3,6 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe HonorsCollegeThesisPresenter do
+  # CCI stub_request
+  stub_request(:get, 'http://ci-test:8080/bigdata/namespace/rw/sparql?GETSTMTS&includeInferred=false&s=%3Chttp://opaquenamespace.org/ns/subject/OregonStateUniversityHonorsCollege%3E').with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Connection' => 'keep-alive', 'Host' => 'ci-test:8080', 'Keep-Alive' => '30', 'User-Agent' => 'Ruby' }).to_return(status: 200, body: '', headers: {})
+
   let(:solr_document) { SolrDocument.new(attributes) }
   let(:ability) { double 'Ability' }
   let(:presenter) { described_class.new(solr_document, ability) }
@@ -15,9 +18,6 @@ RSpec.describe HonorsCollegeThesisPresenter do
       }
     ]
   end
-
-  # CCI stub_request
-  stub_request(:get, "http://ci-test:8080/bigdata/namespace/rw/sparql?GETSTMTS&includeInferred=false&s=%3Chttp://opaquenamespace.org/ns/subject/OregonStateUniversityHonorsCollege%3E").with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Connection' => 'keep-alive', 'Host' => 'ci-test:8080', 'Keep-Alive' => '30', 'User-Agent' => 'Ruby'}).to_return(status: 200, body: "", headers: {})
 
   let(:file) do
     HonorsCollegeThesis.new(id: '123abc',
