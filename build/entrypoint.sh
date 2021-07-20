d29137ca64762b9627fbafefaf68622f89bcdd62
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-07-20 11:07:46 -0700

    precompile assets if we're running in prod/staging

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -29,16 +29,29 @@ if [ "${RAILS_ENV}" == 'production' -o "${RAILS_ENV}" == 'staging' ]; then
   curl -sL https://api.honeycomb.io/1/markers/$HONEYCOMB_DATASET -X POST -H "X-Honeycomb-Team: ${HONEYCOMB_WRITEKEY}" -d "{\"message\":\"${RAILS_ENV} - ${DEPLOYED_VERSION} - Rails booting\", \"type\":\"deploy\"}"
 fi
 
+# Precompile assets
+if [ "${RAILS_ENV}" == 'production' -o "${RAILS_ENV}" == 'staging' ]; then
+   timestamp=`date +'%Y-%m-%d %H:%M:%S'`
+   echo "[$timestamp] Precompiling assets ($RAILS_ENV)"
+   RAILS_ENV=$RAILS_ENV SECRET_KEY_BASE=temporary bundle exec rails assets:precompile
+fi
+
+# Start Puma
 timestamp=`date +'%Y-%m-%d %H:%M:%S'`
 echo "[$timestamp] Starting puma ($RAILS_ENV)"
 RAILS_ENV=$RAILS_ENV bundle exec puma -e ${RAILS_ENV} \
  --dir /data --pidfile $pid_dir/puma.pid -b tcp://0.0.0.0:3000
 
-timestamp=`date +'%Y-%m-%d %H:%M:%S'`
-echo "[$timestamp] Fell through puma, starting life support systems."
-
-while `true`; do
+# Fall through in case puma dies
+if [ "$RAILS_ENV" != "production" ]; then
    timestamp=`date +'%Y-%m-%d %H:%M:%S'`
-   echo "[$timestamp] sleeping..."
-   sleep 30
-done
+   echo "[$timestamp] Fell through puma, starting life support systems."
+
+   while `true`; do
+      timestamp=`date +'%Y-%m-%d %H:%M:%S'`
+      echo "[$timestamp] sleeping..."
+      sleep 30
+   done
+fi
+
+
