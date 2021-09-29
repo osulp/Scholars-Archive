f4293fe428a60b4556d981078780f738548b398b
Corey Gillen <corey.gillen@oregonstate.edu>
2021-09-29 08:03:09 -0700

    Create user for spec and prevent selenium dependency

--- spec/features/read_only_mode_spec.rb
+++ spec/features/read_only_mode_spec.rb
@@ -3,23 +3,25 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.describe 'Read Only Mode', type: :system, integration: true do
-  let(:student) { create :user }
+RSpec.feature 'Read Only Mode', type: :feature do
+  let(:user) do
+    User.new(email: 'test@example.com', username: 'test', guest: false, api_person_updated_at: DateTime.now) { |u| u.save!(validate: false) }
+  end
 
   context 'a logged in user' do
     before do
       login_as user
     end
 
-    it 'View Etd Tabs', js: false do
+    it 'View Articles Tabs', js: false do
       visit('/concern/articles/new')
-      expect(page).to have_content('Submission Checklist')
+      expect(page).to have_content('Add New Article')
       allow(Flipflop).to receive(:read_only?).and_return(true)
       visit('/concern/articles/new')
       expect(page).to have_content('This system is in read-only mode for maintenance.')
       allow(Flipflop).to receive(:read_only?).and_return(false)
       visit('/concern/articles/new')
-      expect(page).to have_content('Submission Checklist')
+      expect(page).to have_content('Add New Article')
     end
   end
 end
