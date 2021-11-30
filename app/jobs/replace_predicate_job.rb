e555f81e350753d15bca3785b120b65e6d9a2e82
Corey Gillen <corey.gillen@oregonstate.edu>
2021-11-30 14:07:34 -0800

    lint

--- app/jobs/replace_predicate_job.rb
+++ app/jobs/replace_predicate_job.rb
@@ -13,33 +13,25 @@ class ReplacePredicateJob < ScholarsArchive::ApplicationJob
     logger.info "Updating #{ids_and_value.count} works"
     logger.info ids_and_value
     ids_and_value.each do |id, value|
-      logger.info value
-      logger.info id
       work = ActiveFedora::Base.find(id)
-      logger.info "Work Found"
       old_predicate_statement = [work.resource.rdf_subject, ::RDF::URI.new(old_predicate), nil]
-      logger.info "Predicate set"
 
-      if !work.resource.graph.query(old_predicate_statement).statements.empty?
-        logger.info "Loading ORM"
+      unless work.resource.graph.query(old_predicate_statement).statements.empty?
         orm = Ldp::Orm.new(work.ldp_source)
 
-        logger.info ""
-        logger.info "Work found #{work.id}. Updating #{metadata_field}"
-
-        logger.info "Setting to new value: #{value}"
+        logger.info "Work found #{work.id}. Updating #{metadata_field}. Setting to new value: #{value}"
         work.send("#{metadata_field}=", value)
 
-        logger.info "Saving work"
+        logger.info 'Saving work'
         if work.save
           logger.info "Work saved. Removing old #{metadata_field}. Pulling graph from fedora"
 
           logger.info "Deleting #{metadata_field} from graph"
           orm.graph.delete(old_predicate_statement)
 
-          logger.info "Saving the graph"
+          logger.info 'Saving the graph'
           if orm.save
-            logger.info "Graph saved successfully"
+            logger.info 'Graph saved successfully'
             counter += 1
           else
             logger.error "Something went wrong with removing old value from graph #{work.id}"
