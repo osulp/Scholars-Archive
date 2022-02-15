5c378a10775997a96d5ae9f4bdfccc77f2a15880
Corey Gillen <corey.gillen@oregonstate.edu>
2022-02-15 11:50:04 -0800

    Remove advanced search type ahead limit

--- app/assets/javascripts/advanced_facets.js
+++ app/assets/javascripts/advanced_facets.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.advanced-search-facet-select').chosen({
     allow_single_deselect: true,
-    max_shown_results: 10,
     no_results_text: 'No results matched'
     });
 });
