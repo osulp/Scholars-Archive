a1319b78bb3aad83ae0c71555664295440c9278f
Corey Gillen <corey.gillen@oregonstate.edu>
2022-03-01 12:18:27 -0800

    Add file name field to file versioning form

--- app/assets/javascripts/file_set_versioning.js
+++ app/assets/javascripts/file_set_versioning.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
   $('#file_set_files').on('input', function(event) {
     // Replace FileSet title with name of new file
-    $('#file_set_title_').val(event.target.files[0].name);
+    $('#file_set_title').val(event.target.files[0].name);
 
     // Warn the user after the first change and let the warning stay on every change after
-    var warningElem = '<div id="title-warning" class="alert alert-warning" role="alert"><strong>Warning:</strong> The title of this fileset has changed to match the above file name.<br/><a href="#descriptions_display">Check the description tab to verify your title</a></div>';
+    var warningElem = '<div id="title-warning" class="alert alert-warning" role="alert"><strong>Warning:</strong> The title of this fileset has changed to match the file name.<br/>Please verify your title above</div>';
     if ($('#title-warning').length == 0) {
       $(event.currentTarget).parents('form').after(warningElem);
     }
