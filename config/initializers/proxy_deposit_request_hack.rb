cd5e0035f9fd4216d621180f36d3e37743d8b62a
straleyb <straleyb@onid.oregonstate.edu>
2022-03-02 08:58:44 -0800

    add other method to hack

--- config/initializers/proxy_deposit_request_hack.rb
+++ config/initializers/proxy_deposit_request_hack.rb
@@ -6,4 +6,15 @@
     message = "#{user_link} wants to transfer a work to you. Review all #{transfer_link}"
     Hyrax::MessengerService.deliver(sending_user, receiving_user, message, "Ownership Change Request")
   end
+
+  def send_request_transfer_message_as_part_of_update
+	message = I18n.t('hyrax.notifications.proxy_deposit_request.transfer_on_update.message', status: status)
+	if receiver_comment.present?
+	  message += " " + I18n.t('hyrax.notifications.proxy_deposit_request.transfer_on_update.comments',
+				  receiver_comment: receiver_comment)
+	end
+	Hyrax::MessengerService.deliver(receiving_user, sending_user, message,
+					I18n.t('hyrax.notifications.proxy_deposit_request.transfer_on_update.subject',
+					       status: status))
+      end
 end
