6f974c6fecafb79509a0a088c329efbd65e218ca
Corey Gillen <corey.gillen@oregonstate.edu>
2022-04-25 14:51:34 -0700

    Missed a presenter

--- app/presenters/default_presenter.rb
+++ app/presenters/default_presenter.rb
@@ -4,8 +4,4 @@
 #  `rails generate hyrax:work Default`
 class DefaultPresenter < Hyrax::WorkShowPresenter
   include ScholarsArchive::DefaultPresenterBehavior
-  # Adds behaviors for hyrax-doi plugin.
-  include Hyrax::DOI::DOIPresenterBehavior
-  # Adds behaviors for DataCite DOIs via hyrax-doi plugin.
-  include Hyrax::DOI::DataCiteDOIPresenterBehavior
 end
