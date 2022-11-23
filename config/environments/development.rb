93ddb0fe5cd54c3e01e0e53774d9704139815506
lamtu1 <tu.lam@oregonstate.edu>
2022-11-23 10:29:14 -0800

    Another progress and add new field to log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,17 +66,32 @@ Rails.application.configure do
     env.register_engine '.haml', Tilt::HamlTemplate
   end
 
-  #config.log_formatter = ::Logger::Formatter.new
+  # Add new customize tags to append to log info
+  config.log_tags = [
+    :request_id,
+    -> request { request.fullpath }
+  ]
 
-  # Add new customize
-  config.log_tags = [:request_id]
+  # Add in new formatter to the already-exist Logger
   config.log_formatter = proc do |severity, datetime, progname, msg|
-   %Q| {
+    # Setup the Logger to get info out of the string and remove any info that has been fetch out
+    msg_vals = []
+    item_arr = msg.split
+    while item_arr[0].include?(']')
+      msg_vals << item_arr[0]
+      item_arr = item_arr[1..-1]
+    end
+    msg = item_arr.join(' ')
+
+    # Format the whole log into JSON format
+    %Q| {
          date: "#{datetime.to_s}",
          log_level: "#{severity.to_s}",
-         req_id: "#{msg.split.first if msg.split.first.include?("]")}",
-         message: "#{msg.split.first.include?("]") ? msg.gsub(msg.split.first, "") : msg}",
-         service_id: "#{msg.split("Service") if msg.split.include?(": ")}"
+         req_id: "#{vals[0]}",
+         uri: "#{URI.regexp([/https?/]).match(msg).to_s.gsub(/[\)\(]*$/, '')}",
+         req_uri: "#{vals[1]}",
+         message: "#{msg}",
+         service_id: "#{msg.split("Service: ").last if msg.split("Service: ").count > 1}"
         }\n|
   end
 
--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,13 +3,7 @@
 if %w[development test].include? Rails.env
   Rails.application.configure do
     config.lograge.enabled = true
-    config.lograge.custom_options = lambda do |event|
-      exceptions = %w[controller action format id]
-      {
-        params: event.payload[:params].except(*exceptions),
-        time: event.time
-      }
-    end
+    
     config.lograge.keep_original_rails_log = true
     config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge-#{Rails.env}.log"
   end
