dd1ea28e65ebf3b5a3e715bf0f7a154a75c0d76f
lamtu1 <tu.lam@oregonstate.edu>
2022-12-12 10:53:08 -0800

    Add in testing env to FixityMailer

---
+++ spec/mailers/scholars_archive/fixity_mailer_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal:true
+
+# RSPEC: Create a testing environment to Fixity Mailer class
+RSpec.describe ScholarsArchive::FixityMailer do
+  # VARIABLES: Create couple variables for testing purpose
+  let(:user) { User.new(email: 'test@email.com') }
+  let(:tst_data) { {start_time: Time.now,
+                    end_time: Time.now,
+                    file_check: 21,
+                    file_pass: 18,
+                    file_fail: 3,
+                    fail_arr: ["1", "2", "3"]} }
+  let(:mail) { described_class.with(user: user, data: tst_data).report_email }
+
+  # TEST GROUP: Create couple test to see if the mailer class pass the test
+  it { expect(mail.subject).to eql('Scholars Archive: Fixity Report') }
+  it { expect(mail.to).to eql([user.email]) }
+end
