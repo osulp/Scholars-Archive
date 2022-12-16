6801077d8875ca16da05bc672375b597b10bfdd9
lamtu1 <tu.lam@oregonstate.edu>
2022-12-16 14:20:07 -0800

    3rd batch of removing lograge

--- sa_scripts/change_to_hct_adminset_and_model.rb
+++ sa_scripts/change_to_hct_adminset_and_model.rb
@@ -8,7 +8,6 @@
 # Rails Console (local):
 
 datetime_today = DateTime.now.strftime('%Y-%m-%d_%H-%M-%p') # "2017-10-27_12-59-PM"
-logger = ActiveSupport::Logger.new("#{Rails.root}/log/change_to_hct_adminset_and_model-#{datetime_today}.log")
 
 counter = 0
 File.readlines('change_to_hct_adminset_and_model_ids.txt').each do |id|
@@ -23,13 +22,13 @@ begin
   cmd_success = system("curl -X PATCH -H 'Content-Type: application/sparql-update' --data-binary '@change_to_hct_adminset_and_model.rdf' #{fedora_item_path}")
 
   counter += 1
-  logger.info "Changed to HCT AdminSet and Model for work #{id}"
+  Rails.logger.info "Changed to HCT AdminSet and Model for work #{id}"
 rescue => e
-  logger.error "\tFailed to change to HCT AdminSet or Model for work #{id}: #{e.message}"
+  Rails.logger.error "\tFailed to change to HCT AdminSet or Model for work #{id}: #{e.message}"
 end
 
 sleep 2
 
 end
 
-logger.info "Done. Changed a total of #{counter} works."
+Rails.logger.info "Done. Changed a total of #{counter} works."
