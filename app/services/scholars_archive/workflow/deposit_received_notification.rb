e3fe2503cf0a1dd3a8ba90e04c43b4a37b17d597
lamtu1 <tu.lam@oregonstate.edu>
2023-02-24 16:24:49 -0800

    Add in new message to auto email

--- app/services/scholars_archive/workflow/deposit_received_notification.rb
+++ app/services/scholars_archive/workflow/deposit_received_notification.rb
@@ -15,6 +15,14 @@ module ScholarsArchive
       def message
         if !SolrDocument.find(work_id)['resource_type_tesim'].include?('Dataset')
           "ScholarsArchive@OSU has received your deposit: #{title} (#{link_to work_id, citeable_url}). Your item is under review by repository administrators. You will be notified if your deposit requires additional changes and/or when your deposit is live in the repository. \n\n #{comment}"
+          "<br /> If the submitted dataset includes human subjects data, you will need to facilitate the following information to the data curator:\n"
+          "* A copy of the IRB approval.
+           <br />
+           * A copy of the application or protocol.
+           <br />
+           * A copy of the consent document, if any.
+           <br />
+           * If the protocol and/or consent document says that the data shared will be de-identified, then there should be less than 3 indirect identifiers in the data files."
         else
           "ScholarsArchive@OSU has received your deposit: #{title} (#{link_to work_id, citeable_url}). Your item is under review by repository administrators. You will be notified if your deposit requires additional changes and/or when your deposit is live in the repository.<br />
            <br />
