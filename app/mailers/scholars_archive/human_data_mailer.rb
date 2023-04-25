c90d37287f358ec7b5c9ffc6a4d9f654be85a339
lamtu1 <tu.lam@oregonstate.edu>
2023-04-25 12:25:51 -0700

    Add fix to email

--- app/mailers/scholars_archive/human_data_mailer.rb
+++ app/mailers/scholars_archive/human_data_mailer.rb
@@ -6,10 +6,10 @@ module ScholarsArchive
     # METHOD: Create an email and send a report to the reviewer
     def email_on_human_data
       @human_data_info = params[:data]
-      @email_recipients = [Hyrax.config.contact_email, 'imholts@oregonstate.edu', 'keyca@oregonstate.edu', 'llebotlc@oregonstate.edu']
+      @emails = params[:user_mail]
 
       # MAIL: Email out to couple recipients
-      mail(to: @email_recipients, subject: 'Dataset Alert! Human Subject Data Included')
+      mail(to: @emails, subject: 'Dataset Alert! Human Subject Data Included')
     end
   end
 end
