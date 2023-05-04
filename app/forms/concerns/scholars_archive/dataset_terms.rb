a29bf08ad732564a45b45cf12eaaed19ed23c332
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2023-05-04 10:02:41 -0700

    Update dataset_terms.rb

--- app/forms/concerns/scholars_archive/dataset_terms.rb
+++ app/forms/concerns/scholars_archive/dataset_terms.rb
@@ -6,45 +6,45 @@ module ScholarsArchive
     def self.base_terms
       self.primary_terms + ::ScholarsArchive::DefaultTerms.date_terms +
       %i[nested_related_items
-        date_uploaded
-        date_modified
-        funding_body
-        tableofcontents
-        file_extent
-        file_format
-        dspace_community
-        dspace_collection
-        isbn
-        issn
-        embargo_reason
-        human_data]
+         date_uploaded
+         date_modified
+         funding_body
+         tableofcontents
+         file_extent
+         file_format
+         dspace_community
+         dspace_collection
+         isbn
+         issn
+         embargo_reason
+         human_data]
     end
 
     def self.primary_terms
       %i[nested_ordered_title
-        alt_title
-        nested_ordered_creator
-        academic_affiliation
-        other_affiliation
-        nested_ordered_contributor
-        nested_ordered_abstract
-        license
-        resource_type
-        doi
-        dates_section
-        bibliographic_citation
-        in_series
-        subject
-        rights_statement
-        hydrologic_unit_code
-        geo_section
-        funding_statement
-        publisher
-        peerreviewed
-        language
-        digitization_spec
-        replaces
-        nested_ordered_additional_information]
+         alt_title
+         nested_ordered_creator
+         academic_affiliation
+         other_affiliation
+         nested_ordered_contributor
+         nested_ordered_abstract
+         license
+         resource_type
+         doi
+         dates_section
+         bibliographic_citation
+         in_series
+         subject
+         rights_statement
+         hydrologic_unit_code
+         geo_section
+         funding_statement
+         publisher
+         peerreviewed
+         language
+         digitization_spec
+         replaces
+         nested_ordered_additional_information]
     end
   end
 end
