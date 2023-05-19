dc8e9ad06983ec7479bfe8ce7fe593811bdaa4e4
straleyb <straleyb@onid.oregonstate.edu>
2023-05-19 11:50:47 -0700

    updates parentheses

--- app/models/concerns/scholars_archive/has_nested_ordered_properties.rb
+++ app/models/concerns/scholars_archive/has_nested_ordered_properties.rb
@@ -91,31 +91,31 @@ module ScholarsArchive
       # output: [["Another Title", "1"], ["My Title", "0"]]
       def validate_titles
         nested_ordered_title.select { |i| (i.instance_of? NestedOrderedTitle) && (i.index.first.present? && i.title.first.present?) && (i.index.first.instance_of? String) && (i.title.first.instance_of? String) }
-          .map { |i| i.instance_of? NestedOrderedTitle ? [i.title.first, i.index.first] : [i] }
+          .map { |i| i.instance_of?(NestedOrderedTitle) ? [i.title.first, i.index.first] : [i] }
           .select(&:present?)
       end
 
       def validate_creators
         nested_ordered_creator.select { |i| (i.instance_of? NestedOrderedCreator) && (i.index.first.present? && i.creator.first.present?) && (i.index.first.instance_of? String) && (i.creator.first.instance_of? String) }
-          .map { |i| i.instance_of? NestedOrderedCreator ? [i.creator.first, i.index.first] : [i] }
+          .map { |i| i.instance_of?(NestedOrderedCreator) ? [i.creator.first, i.index.first] : [i] }
           .select(&:present?)
       end
 
       def validate_abstracts
         nested_ordered_abstract.select { |i| (i.instance_of? NestedOrderedAbstract) && (i.index.first.present? && i.abstract.first.present?) && (i.index.first.instance_of? String) && (i.abstract.first.instance_of? String) }
-          .map { |i| i.instance_of? NestedOrderedAbstract ? [i.abstract.first, i.index.first] : [i] }
+          .map { |i| i.instance_of?(NestedOrderedAbstract) ? [i.abstract.first, i.index.first] : [i] }
           .select(&:present?)
       end
 
       def validate_contributors
         nested_ordered_contributor.select { |i| (i.instance_of? NestedOrderedContributor) && (i.index.first.present? && i.contributor.first.present?) && (i.index.first.instance_of? String) && (i.contributor.first.instance_of? String) }
-          .map { |i| i.instance_of? NestedOrderedContributor ? [i.contributor.first, i.index.first] : [i] }
+          .map { |i| i.instance_of?(NestedOrderedContributor) ? [i.contributor.first, i.index.first] : [i] }
           .select(&:present?)
       end
 
       def validate_info
         nested_ordered_additional_information.select { |i| (i.instance_of? NestedOrderedAdditionalInformation) && (i.index.first.present? && i.additional_information.first.present?) && (i.index.first.instance_of? String) && (i.additional_information.first.instance_of? String) }
-          .map { |i| i.instance_of? NestedOrderedAdditionalInformation ? [i.additional_information.first, i.index.first] : [i] }
+          .map { |i| i.instance_of?(NestedOrderedAdditionalInformation) ? [i.additional_information.first, i.index.first] : [i] }
           .select(&:present?)
       end
     end
