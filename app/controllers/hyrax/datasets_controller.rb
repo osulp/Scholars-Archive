14305b117a34c8ea67015b43a653dcaa69d9827a
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2023-06-01 11:40:54 -0700

    Update datasets_controller.rb

--- app/controllers/hyrax/datasets_controller.rb
+++ app/controllers/hyrax/datasets_controller.rb
@@ -27,7 +27,7 @@ module Hyrax
     def redirect_if_embargo
       curation_concern = ActiveFedora::Base.find(params[:id])
 			# First we check if the user can see the work
-      return unless cannot?(:read, curation_concern) && curation_concern.embargo_id.present?
+			return unless cannot?(:read, curation_concern) && curation_concern.embargo_id.present?
 
 			# Next we check if user got here specifically from the homepage. This means they got redirected and clicked the login link.
 			return if request.original_url == "http://ir.library.oregonstate.edu/"
@@ -36,12 +36,12 @@ module Hyrax
 			# and if its OSU visible, provided a link to login and continue to where they were going 
 			case curation_concern.embargo.visibility_during_embargo
 			when 'restricted'
-      	flash[:notice] = "The item you are trying to access is under embargo until #{curation_concern.embargo.embargo_release_date.month} #{curation_concern.embargo.embargo_release_date.day}, #{curation_concern.embargo.embargo_release_date.year}."
+				flash[:notice] = "The item you are trying to access is under embargo until #{curation_concern.embargo.embargo_release_date.month} #{curation_concern.embargo.embargo_release_date.day}, #{curation_concern.embargo.embargo_release_date.year}."
 			when 'authenticated'
-      	flash[:notice] = "The item you are trying to access is under embargo until #{curation_concern.embargo.embargo_release_date.month} #{curation_concern.embargo.embargo_release_date.day}, #{curation_concern.embargo.embargo_release_date.year}. However, users with an OSU login (ONID) may log in to view the item. #{"<a href=#{request.original_url}> Click here to login and continue to your work </a>".html_safe}"
+				flash[:notice] = "The item you are trying to access is under embargo until #{curation_concern.embargo.embargo_release_date.month} #{curation_concern.embargo.embargo_release_date.day}, #{curation_concern.embargo.embargo_release_date.year}. However, users with an OSU login (ONID) may log in to view the item. #{"<a href=#{request.original_url}> Click here to login and continue to your work </a>".html_safe}"
 			end
 
-      redirect_to '/'
+			redirect_to '/'
     end
   end
 end
