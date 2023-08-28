353822852c65a1ab4862bf5c798d8231a3f617bd
lamtu1 <tu.lam@oregonstate.edu>
2023-08-28 15:17:39 -0700

    Fix rubo

--- spec/views/hyrax/admin/workflows/index.html.erb_spec.rb
+++ spec/views/hyrax/admin/workflows/index.html.erb_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'hyrax/admin/workflows/index.html.erb', type: :view do
 
   context 'when user has submissions to review' do
     it 'displays submission date in the format YYYY-MM-DD HH:MM:SS' do
-      expect(rendered).to include('2019-04-24 21:22:44')
+      expect(rendered).to include('2019-04-24 14:22:44')
     end
   end
 end
