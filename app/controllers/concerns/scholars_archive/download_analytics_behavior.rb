8e856792cd5b3529fe637bda2926021c0f435a25
Corey Gillen <corey.gillen@oregonstate.edu>
2023-09-11 13:45:54 -0700

    Filter thumbnail download analytics

--- app/controllers/concerns/scholars_archive/download_analytics_behavior.rb
+++ app/controllers/concerns/scholars_archive/download_analytics_behavior.rb
@@ -9,7 +9,7 @@ module ScholarsArchive
       after_action :track_download, only: :show
 
       def track_download
-        unless Hyrax.config.google_analytics_id.blank?
+        unless Hyrax.config.google_analytics_id.blank? || params[:file].to_s.downcase == 'thumbnail'
           # Staccato works with Google Analytics v1 api:
           # https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
           # Staccato on Github: https://github.com/tpitale/staccato
