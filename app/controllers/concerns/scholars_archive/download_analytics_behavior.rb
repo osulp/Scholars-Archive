b5c17c11c2578e6c3000334da14837030118b1dd
lamtu1 <111306182+lamtu1@users.noreply.github.com>
2023-10-17 11:33:02 -0700

    Add in a filter to not collect admin visit (#2538)

--- app/controllers/concerns/scholars_archive/download_analytics_behavior.rb
+++ app/controllers/concerns/scholars_archive/download_analytics_behavior.rb
@@ -9,7 +9,8 @@ module ScholarsArchive
       after_action :track_download, only: :show
 
       def track_download
-        unless Hyrax.config.google_analytics_id.blank? || params[:file].to_s.downcase == 'thumbnail'
+        # Add in special case to ignore collection of analytic data
+        unless Hyrax.config.google_analytics_id.blank? || params[:file].to_s.downcase == 'thumbnail' || request.path.include?('/admin/')
           # Staccato works with Google Analytics v1 api:
           # https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
           # Staccato on Github: https://github.com/tpitale/staccato
