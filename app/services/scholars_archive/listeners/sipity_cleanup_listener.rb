1a3fb018b4faae0eaa5487c541afbeb9e9d631cd
Corey Gillen <corey.gillen@oregonstate.edu>
2024-01-19 12:56:22 -0800

    Lint for sipity entity deletion listener

--- app/services/scholars_archive/listeners/sipity_cleanup_listener.rb
+++ app/services/scholars_archive/listeners/sipity_cleanup_listener.rb
@@ -3,7 +3,7 @@
 module ScholarsArchive
   module Listeners
     ##
-    # Listens for object deleted events and cleans up associated members
+    # Listens for object deleted events and cleans up associated Sipity Entity + Sipity Entity Specific Responsibilities
     class SipityCleanupListener
       # Called when 'object.deleted' event is published
       # @param [Dry::Events::Event] event
@@ -12,14 +12,13 @@ module ScholarsArchive
         sipity_entity = Sipity::Entity.find_by('proxy_for_global_id like ?', "%/#{event[:id]}")
         Hyrax.logger.info("Deleting Sipity Entity Responsibilities for #{event[:id]}")
         sipity_entity.entity_specific_responsibilities.map(&:delete)
-        Hyrax.logger.info("Sipity Entity Responsibilities for #{event[:id]} Deleted")
-        Hyrax.logger.info("Deleting Sipity Entity #{event[:id]}")
+        Hyrax.logger.info("Sipity Entity Responsibilities for #{event[:id]} deleted now deleting Sipity Entity #{event[:id]}")
         sipity_entity.delete
         Hyrax.logger.info("Sipity Entity #{event[:id]} Deleted")
-      rescue StandardError => err
+      rescue StandardError => e
         Hyrax.logger.warn "Failed to delete sipity entity for #{event[:id]}. " \
                           'This entity might be orphaned.' \
-                          "\n\t#{err.message}"
+                          "\n\t#{e.message}"
       end
     end
   end
