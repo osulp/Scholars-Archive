0f1ab9c025b5bf2c2ce977ba57a99df1cbb5f1b5
Corey Gillen <corey.gillen@oregonstate.edu>
2024-05-21 14:48:10 -0700

    sort_field wasn't the issue, Hyrax#5905 implements a fix for collection show page sorting

--- config/initializers/hacks/hyrax_collection_search_builder_hack.rb
+++ config/initializers/hacks/hyrax_collection_search_builder_hack.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal:true
 
+# Implement Fix https://github.com/samvera/hyrax/pull/5920 from Hyrax v4. No longer necessary after >v4 upgrade
 Hyrax::CollectionSearchBuilder.class_eval do
-  # Change from title_si to title_ssort since that's the field we use to sort
-  # @return [String] Solr field name indicating default sort order
-  def sort_field
-    "title_ssort"
+  # Sort results by title if no query was supplied.
+  # This overrides the default 'relevance' sort.
+  def add_sorting_to_solr(solr_parameters)
+    return if solr_parameters[:q]
+    solr_parameters[:sort] ||= sort
+    solr_parameters[:sort] ||= "#{sort_field} asc"
   end
 end
