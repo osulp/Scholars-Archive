0d64ca978ee17887ef62d9570f0dd1b0cc88f5de
lamtu1 <tu.lam@oregonstate.edu>
2024-06-05 14:25:09 -0700

    one more fix

--- app/mailers/scholars_archive/container_mailer.rb
+++ app/mailers/scholars_archive/container_mailer.rb
@@ -5,7 +5,7 @@ module ScholarsArchive
   class ContainerMailer < ApplicationMailer
     # METHOD: Create an email and send a report to the user
     def report_email
-      #attachments['filesets_inventory.txt'] = File.read('./tmp/inventory.txt')
+      # attachments['filesets_inventory.txt'] = File.read('./tmp/inventory.txt')
       mail(to: params[:to], subject: 'Scholars Archive: Archive/Container Files Inventory Report')
     end
   end
