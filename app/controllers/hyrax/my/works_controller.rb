fa493dc205cc425297cf8f8a2607ff29431b7030
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2024-07-11 12:04:30 -0700

    Update works_controller.rb

--- app/controllers/hyrax/my/works_controller.rb
+++ app/controllers/hyrax/my/works_controller.rb
@@ -1,53 +1,55 @@
 # frozen_string_literal: true
+
 module Hyrax
-    module My
-      class WorksController < MyController
-        # Define collection specific filter facets.
-        def self.configure_facets
-          configure_blacklight do |config|
-            config.search_builder_class = Hyrax::My::WorksSearchBuilder
-            config.add_facet_field "admin_set_sim", limit: 5
-            config.add_facet_field "member_of_collections_ssim", limit: 5
-          end
+  module My
+    # MY WORKS CONTROLLER
+    class WorksController < MyController
+      # Define collection specific filter facets.
+      def self.configure_facets
+        configure_blacklight do |config|
+          config.search_builder_class = Hyrax::My::WorksSearchBuilder
+          config.add_facet_field 'admin_set_sim', limit: 5
+          config.add_facet_field 'member_of_collections_ssim', limit: 5
         end
-        configure_facets
+      end
+      configure_facets
 
-        class_attribute :create_work_presenter_class
-        self.create_work_presenter_class = Hyrax::SelectTypeListPresenter
+      class_attribute :create_work_presenter_class
+      self.create_work_presenter_class = Hyrax::SelectTypeListPresenter
 
-        def index
-          # The user's collections for the "add to collection" form
-          @user_collections = collections_service.search_results(:deposit)
+      def index
+        # The user's collections for the "add to collection" form
+        @user_collections = collections_service.search_results(:deposit)
 
-          add_breadcrumb t(:'hyrax.controls.home'), root_path
-          add_breadcrumb t(:'hyrax.dashboard.breadcrumbs.admin'), hyrax.dashboard_path
-          add_breadcrumb t(:'hyrax.admin.sidebar.works'), hyrax.my_works_path
-          managed_works_count
-          @create_work_presenter = create_work_presenter_class.new(current_user)
-          super
-        end
+        add_breadcrumb t(:'hyrax.controls.home'), root_path
+        add_breadcrumb t(:'hyrax.dashboard.breadcrumbs.admin'), hyrax.dashboard_path
+        add_breadcrumb t(:'hyrax.admin.sidebar.works'), hyrax.my_works_path
+        managed_works_count
+        @create_work_presenter = create_work_presenter_class.new(current_user)
+        super
+      end
 
-        private
+      private
 
-        # OVERRIDE FROM HYRAX: CAN REMOVE AT 4.0
-        def collections_service
-          dupe = dup
-          dupe.params = {}
-          Hyrax::CollectionsService.new(dupe)
-        end
+      # OVERRIDE FROM HYRAX: CAN REMOVE AT 4.0
+      def collections_service
+        dupe = dup
+        dupe.params = {}
+        Hyrax::CollectionsService.new(dupe)
+      end
 
-        def search_action_url(*args)
-          hyrax.my_works_url(*args)
-        end
+      def search_action_url(*args)
+        hyrax.my_works_url(*args)
+      end
 
-        # The url of the "more" link for additional facet values
-        def search_facet_path(args = {})
-          hyrax.my_dashboard_works_facet_path(args[:id])
-        end
+      # The url of the "more" link for additional facet values
+      def search_facet_path(args = {})
+        hyrax.my_dashboard_works_facet_path(args[:id])
+      end
 
-        def managed_works_count
-          @managed_works_count = Hyrax::Works::ManagedWorksService.managed_works_count(scope: self)
-        end
+      def managed_works_count
+        @managed_works_count = Hyrax::Works::ManagedWorksService.managed_works_count(scope: self)
       end
     end
   end
+end
