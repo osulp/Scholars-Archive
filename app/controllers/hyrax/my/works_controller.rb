4ef43ffc1f137f755306062a054231b9a64743ea
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2024-07-11 11:55:33 -0700

    Update works_controller.rb

--- app/controllers/hyrax/my/works_controller.rb
+++ app/controllers/hyrax/my/works_controller.rb
@@ -11,14 +11,14 @@ module Hyrax
           end
         end
         configure_facets
-  
+
         class_attribute :create_work_presenter_class
         self.create_work_presenter_class = Hyrax::SelectTypeListPresenter
-  
+
         def index
           # The user's collections for the "add to collection" form
           @user_collections = collections_service.search_results(:deposit)
-  
+
           add_breadcrumb t(:'hyrax.controls.home'), root_path
           add_breadcrumb t(:'hyrax.dashboard.breadcrumbs.admin'), hyrax.dashboard_path
           add_breadcrumb t(:'hyrax.admin.sidebar.works'), hyrax.my_works_path
@@ -26,25 +26,25 @@ module Hyrax
           @create_work_presenter = create_work_presenter_class.new(current_user)
           super
         end
-  
+
         private
 
         # OVERRIDE FROM HYRAX: CAN REMOVE AT 4.0
         def collections_service
-          dupe = self.dup
+          dupe = dup
           dupe.params = {}
           Hyrax::CollectionsService.new(dupe)
         end
-  
+
         def search_action_url(*args)
           hyrax.my_works_url(*args)
         end
-  
+
         # The url of the "more" link for additional facet values
         def search_facet_path(args = {})
           hyrax.my_dashboard_works_facet_path(args[:id])
         end
-  
+
         def managed_works_count
           @managed_works_count = Hyrax::Works::ManagedWorksService.managed_works_count(scope: self)
         end
