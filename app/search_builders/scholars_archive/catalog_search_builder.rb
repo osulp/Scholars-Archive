d39f59b81155cc809f686ccec57ba8d1da008973
straleyb <straleyb@onid.oregonstate.edu>
2024-07-15 10:33:11 -0700

    Remove config for normal search boxes to use AND/OR

--- app/search_builders/scholars_archive/catalog_search_builder.rb
+++ app/search_builders/scholars_archive/catalog_search_builder.rb
@@ -7,5 +7,5 @@ class ScholarsArchive::CatalogSearchBuilder < Hyrax::CatalogSearchBuilder
   include BlacklightRangeLimit::RangeLimitBuilder
   include Hydra::AccessControlsEnforcement
   include Hyrax::SearchFilters
-  self.default_processor_chain += %i[add_advanced_parse_q_to_solr add_advanced_search_to_solr]
+  self.default_processor_chain += %i[add_advanced_search_to_solr]
 end
