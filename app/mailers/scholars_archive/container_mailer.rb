341b0ab84bb300f468396e053ed510b3a4cb1b67
lamtu1 <111306182+lamtu1@users.noreply.github.com>
2024-09-19 11:23:03 -0700

    Add in zip content to filesets inventory (#2685)

--- app/mailers/scholars_archive/container_mailer.rb
+++ app/mailers/scholars_archive/container_mailer.rb
@@ -5,8 +5,7 @@ module ScholarsArchive
   class ContainerMailer < ApplicationMailer
     # METHOD: Create an email and send a report to the user
     def report_email
-      attachments['filesets_inventory.csv'] = File.read('./tmp/filesets_inventory.csv') if File.exist?('./tmp/filesets_inventory.csv')
-      attachments['filesets_failed.csv'] = File.read('./tmp/filesets_failed.csv') if File.exist?('./tmp/filesets_failed.csv')
+      attachments['filesets_inventory.zip'] = File.read('./tmp/filesets_inventory.zip') if File.exist?('./tmp/filesets_inventory.zip')
       mail(to: params[:to], subject: 'Scholars Archive: Archive/Container Files Inventory Report')
     end
   end
