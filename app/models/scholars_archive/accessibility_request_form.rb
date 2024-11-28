2625f86d0452bb005ea5b423bddcbf29cf7dcc65
lamtu1 <tu.lam@oregonstate.edu>
2024-11-27 16:00:16 -0800

    Add in the model form

---
+++ app/models/scholars_archive/accessibility_request_form.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module ScholarsArchive
+  # CLASS: Accessibility Request Form Model
+  class AccessibilityRequestForm
+    include ActiveModel::Model
+    # ADD: Add in accessors to map out on field that will be use in the form
+    attr_accessor :accessibility_request_method, :email, :name, :phone, :url_link, :request_detail, :additional_detail, :date
+
+    # VALIDATION: Add in validation to these variables to check before pass the form
+    validates :email, :name, :phone, :url_link, :request_detail, :additional_detail, :date, presence: true
+    validates :email, format: /\A([\w.%+-]+)@([\w-]+\.)+(\w{2,})\z/i, allow_blank: true
+
+    # SPAM: Check to make sure this section isn't fill, if so, it might be a spam
+    def spam?
+      accessiblity_request_method.present?
+    end
+
+    # HEADER: Declare the e-mail headers. It accepts anything the mail method in ActionMailer accepts
+    def headers
+      {
+        subject: 'Scholars Archive Accessibility Request Form: Request on Work',
+        to: Hyrax.config.contact_email,
+        from: email
+      }
+    end
+  end
+end
