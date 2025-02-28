3fb98c7b6931521a20da95a7bf373a6a77d6b838
lamtu1 <tu.lam@oregonstate.edu>
2025-02-28 14:25:37 -0800

    Add one more spec to service

--- spec/services/scholars_archive/accessibility_feature_service_spec.rb
+++ spec/services/scholars_archive/accessibility_feature_service_spec.rb
@@ -10,4 +10,10 @@ describe ScholarsArchive::AccessibilityFeatureService do
       expect(service.select_active_options).to include(%w[annotations annotations], %w[alternativeText alternativeText], %w[taggedPDF taggedPDF], %w[openCaptions openCaptions])
     end
   end
+
+  describe '#all_labels' do
+    it 'returns active terms' do
+      expect(service.all_labels('annotations')).to eq ['annotations']
+    end
+  end
 end
