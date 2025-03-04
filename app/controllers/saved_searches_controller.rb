8770fe7a6068c5a0b709d969187816ef7b8cf72e
Corey Gillen <corey.gillen@oregonstate.edu>
2025-03-04 12:34:42 -0800

    Remove blacklight saved searches controller

--- app/controllers/saved_searches_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# saved searches controllers
-class SavedSearchesController < ApplicationController
-  include Blacklight::SavedSearches
-  helper BlacklightAdvancedSearch::RenderConstraintsOverride
-end
