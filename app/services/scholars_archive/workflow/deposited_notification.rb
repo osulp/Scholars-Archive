fc1afe1da44617317d00e79584e61db80b019b1e
straleyb <straleyb@onid.oregonstate.edu>
2025-03-05 11:34:19 -0800

    use our deposited notification

--- app/services/scholars_archive/workflow/deposited_notification.rb
+++ app/services/scholars_archive/workflow/deposited_notification.rb
@@ -4,6 +4,11 @@ module ScholarsArchive
   module Workflow
     # Deposit notification
     class DepositedNotification < ScholarsArchive::Workflow::AbstractNotification
+      def initialize(entity, comment, user, recipients)
+        @doi = entity.proxy_for.doi
+        super(entity, comment, user, recipients)
+      end
+
       private
 
       def subject
@@ -11,7 +16,7 @@ module ScholarsArchive
       end
 
       def message
-        "Your deposit: #{title} (#{link_to work_id, citeable_url}) was approved by #{user.user_key} and is now live in ScholarsArchive@OSU. #{comment} \n\n Citeable URL: #{citeable_url}"
+        "Your deposit: #{title} DOI:#{doi} (#{link_to work_id, citeable_url}) was approved by #{user.user_key} and is now live in ScholarsArchive@OSU. #{comment} \n\n Citeable URL: #{citeable_url}"
       end
 
       def users_to_notify
