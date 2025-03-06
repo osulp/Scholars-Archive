56ce9c033d8486e72c059b2179dea07755dab4e5
straleyb <straleyb@onid.oregonstate.edu>
2025-03-06 07:16:50 -0800

    updates proper notification

--- app/services/scholars_archive/workflow/deposit_received_notification.rb
+++ app/services/scholars_archive/workflow/deposit_received_notification.rb
@@ -25,7 +25,7 @@ module ScholarsArchive
         elsif !SolrDocument.find(work_id)['resource_type_tesim']&.include?('Dataset')
           "ScholarsArchive@OSU has received your deposit: #{title} (#{link_to work_id, citeable_url}). Your item is under review by repository administrators. You will be notified if your deposit requires additional changes and/or when your deposit is live in the repository. \n\n #{comment}"
         else
-          "ScholarsArchive@OSU has received your deposit: #{title} (#{link_to work_id, citeable_url}). Your item is under review by repository administrators. You will be notified if your deposit requires additional changes and/or when your deposit is live in the repository.<br />
+          "ScholarsArchive@OSU has received your deposit: #{title} (#{link_to work_id, citeable_url}). Your DOI is: #{SolrDocument.find(work_id).doi} Your item is under review by repository administrators. You will be notified if your deposit requires additional changes and/or when your deposit is live in the repository.<br />
            <br />
            Reviews typically take several days. If you have a deadline that we should know of please send a message to researchdataservices@oregonstate.edu.<br />
            <br />
