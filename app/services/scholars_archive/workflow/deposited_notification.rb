1553c71faca18f488b90ae10883657d126730cfa
Tu Lam <tu.lam@oregonstate.edu>
2025-03-26 15:31:50 -0700

    Change the layout to only remove DOI:

--- app/services/scholars_archive/workflow/deposited_notification.rb
+++ app/services/scholars_archive/workflow/deposited_notification.rb
@@ -16,7 +16,7 @@ module ScholarsArchive
       end
 
       def message
-        "Your deposit: '#{title}' was approved by #{user.user_key} and is now live in ScholarsArchive@OSU. #{comment} \n\n Citeable URL: #{citeable_url}"
+        "Your deposit: '#{title}' #{@doi} (#{link_to work_id, citeable_url}) was approved by #{user.user_key} and is now live in ScholarsArchive@OSU. #{comment} \n\n Citeable URL: #{citeable_url}"
       end
 
       def users_to_notify
