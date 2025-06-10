a5e78ffdb92686d359b47fd5e3a12fc2e6bbb391
straleyb <straleyb@onid.oregonstate.edu>
2025-06-10 08:26:34 -0700

    Remove unnecessary files

--- app/controllers/hyrax/external_datasets_controller.rb
+++ app/controllers/hyrax/external_datasets_controller.rb
@@ -4,14 +4,6 @@
 #  `rails generate hyrax:work_resource ExternalDataset`
 module Hyrax
   # Generated controller for ExternalDataset
-  class ExternalDatasetsController < ApplicationController
-    # Adds Hyrax behaviors to the controller.
-    include Hyrax::WorksControllerBehavior
-    include Hyrax::BreadcrumbsForWorks
-    self.curation_concern_type = ::ExternalDataset
-
-    # Use a Valkyrie aware form service to generate Valkyrie::ChangeSet style
-    # forms.
-    self.work_form_service = Hyrax::FormFactory.new
+  class ExternalDatasetsController < DatasetsController
   end
 end
