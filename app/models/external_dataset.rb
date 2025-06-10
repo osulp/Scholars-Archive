17c930aedd548fb9bddfb2946b137bb695329e76
straleyb <straleyb@onid.oregonstate.edu>
2025-06-10 13:20:29 -0700

    Fix based near attributes

--- app/models/external_dataset.rb
+++ app/models/external_dataset.rb
@@ -2,5 +2,26 @@
 
 # Generated via
 #  `rails generate hyrax:work_resource ExternalDataset`
-class ExternalDataset < Dataset
+class ExternalDataset < ActiveFedora::Base
+  include ::Hyrax::WorkBehavior
+  include ::ScholarsArchive::DefaultMetadata
+  include ::ScholarsArchive::FinalizeNestedMetadata
+  include ScholarsArchive::HasSolrLabels
+  include ScholarsArchive::DateOperations
+  include ScholarsArchive::HasTriplePoweredProperties
+  include ScholarsArchive::ExcludedDefaultLicenses
+  include ScholarsArchive::HasNestedOrderedProperties
+
+  self.indexer = DefaultWorkIndexer
+  # Change this to restrict which works can be added as a child.
+  # self.valid_child_concerns = []
+
+  validates_with ScholarsArchive::Validators::OtherAffiliationValidator
+  validates_with ScholarsArchive::Validators::NestedRelatedItemsValidator
+
+  private
+
+  def set_defaults
+    self.accessibility_feature = ['unknown']
+  end
 end
