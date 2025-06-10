0a0975719f4c8729b6efebc636fd1ee280e30a9b
straleyb <straleyb@onid.oregonstate.edu>
2025-06-10 12:17:44 -0700

    Add in admin set and use proper forms/models

--- app/forms/hyrax/external_dataset_form.rb
+++ app/forms/hyrax/external_dataset_form.rb
@@ -4,6 +4,12 @@
 #  `rails generate hyrax:work ExternalDataset`
 module Hyrax
   # Generated form for ExternalDataset
-  class ExternalDatasetForm < DatasetForm
+  class ExternalDatasetForm < Hyrax::Forms::WorkForm
+    include ::ScholarsArchive::TriplePoweredProperties::TriplePoweredForm
+    include ScholarsArchive::DatasetFormBehavior
+
+    self.model_class = ::ExternalDataset
+    self.field_metadata_service = ScholarsArchive::FormMetadataService
+
   end
 end
--- app/presenters/external_dataset_presenter.rb
+++ app/presenters/external_dataset_presenter.rb
@@ -2,7 +2,5 @@
 
 # Generated via
 #  `rails generate hyrax:work ExternalDataset`
-module Hyrax
-  class ExternalDatasetPresenter < DatasetPresenter
-  end
+class ExternalDatasetPresenter < DefaultPresenter
 end
