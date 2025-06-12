20dad65a0e62ba4b4ff9f727043d8d577f8e6237
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2025-06-12 12:36:51 -0700

    Initial pass through for External Dataset (#2784)
    * Initial pass through for External Dataset
    
    * Remove unnecessary files
    
    * remove bunk metadata
    
    * Update frozen string
    
    * Remove bunk specs
    
    * remove bunk spec
    
    * Add in admin set and use proper forms/models
    
    * Fix human data
    
    * Fix based near attributes
    
    * remove dup method
    
    * lint
    
    * fix spec
    
    * more spec fixes
    
    * Update create_external_dataset_spec.rb
    
    * Generalize remove human data
    
    * Delete logs.txt
    
    * Update spec_helper.rb
    
    * Update _select_work_type_modal.html.erb_spec.rb

---
+++ spec/features/create_external_dataset_spec.rb
@@ -0,0 +1,8 @@
+# Generated via
+#  `rails generate hyrax:work ExternalDataset`
+require 'rails_helper'
+include Warden::Test::Helpers
+
+# NOTE: If you generated more than one work, you have to set "js: true"
+RSpec.feature 'Create a ExternalDataset', js: false do
+end
