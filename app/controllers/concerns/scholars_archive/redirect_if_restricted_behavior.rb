02612708a77d4c121e96508d9fe34b042a4fb0c8
straleyb <straleyb@onid.oregonstate.edu>
2025-08-22 08:49:20 -0700

    DRY redirect_from_homepage

--- app/controllers/concerns/scholars_archive/redirect_if_restricted_behavior.rb
+++ app/controllers/concerns/scholars_archive/redirect_if_restricted_behavior.rb
@@ -14,10 +14,11 @@ module ScholarsArchive
       def redirect_if_restricted
         curation_concern = ActiveFedora::Base.find(params[:id])
         referrer_uri = URI.parse(request.referrer.to_s)
+        referred_from_homepage = referrer_uri.host == ENV.fetch('SCHOLARSARCHIVE_URL_HOST') && referrer_uri.path == '/'
 
         # If we are approving and it is not readable by the current user
         if cannot?(:edit, curation_concern) && (curation_concern.to_solr['workflow_state_name_ssim'] == 'Changes Required')
-          if request.referrer.to_s == "https://#{ENV.fetch('SCHOLARSARCHIVE_URL_HOST')}/"
+          if referred_from_homepage
             authenticate_user!
             return if can?(:edit, curation_concern)
           end
@@ -33,7 +34,7 @@ module ScholarsArchive
         return unless cannot?(:read, curation_concern) && (!visible_under_embargo(curation_concern) || curation_concern.visibility == 'authenticated')
 
         # If one of the referrers is matches with the request referrer then allow login. This runs after the redirect for OSU-ONLY items.
-        return if referrer_uri.host == ENV.fetch('SCHOLARSARCHIVE_URL_HOST') && referrer_uri.path == '/'
+        return if referred_from_homepage
 
         # Otherwise, this returns them to the homepage because they got here from elsewhere and need to know this work is embargoed
         # and if its OSU visible, provided a link to login and continue to where they were going
