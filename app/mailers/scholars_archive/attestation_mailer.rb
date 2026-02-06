4929637bec466571ef8abc0048560181f14f6694
straleyb <straleyb@onid.oregonstate.edu>
2026-02-06 09:24:40 -0800

    remove named params

--- app/mailers/scholars_archive/attestation_mailer.rb
+++ app/mailers/scholars_archive/attestation_mailer.rb
@@ -4,7 +4,7 @@ module ScholarsArchive
   # MAILER: A human subject data mailer application
   class AttestationMailer < ApplicationMailer
     # METHOD: Create an email and send a report to the reviewer
-    def accessibility_attestation_mail(user_email: email, user_work: user_work)
+    def accessibility_attestation_mail(email, user_work)
       @user = email
       @work = user_work
       # MAIL: Email out to couple recipients
--- app/mailers/scholars_archive/user_attestation_mail.rb
+++ app/mailers/scholars_archive/user_attestation_mail.rb
@@ -4,7 +4,7 @@ module ScholarsArchive
   # MAILER: A human subject data mailer application
   class UserAttestationMailer < ApplicationMailer
     # METHOD: Create an email and send a report to the reviewer
-    def user_attestation_mail(user_email: email)
+    def user_attestation_mail(email)
       @user = email
       # MAIL: Email out to couple recipients
       mail(to: @user, subject: 'Work Needs Accessibility Support.')
