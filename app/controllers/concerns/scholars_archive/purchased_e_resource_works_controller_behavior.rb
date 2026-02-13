dfae9c1d99f7da7e87e34b90d45484c044c0c703
straleyb <straleyb@onid.oregonstate.edu>
2026-02-13 11:02:05 -0800

    Move emailer over to controller

--- app/controllers/concerns/scholars_archive/purchased_e_resource_works_controller_behavior.rb
+++ app/controllers/concerns/scholars_archive/purchased_e_resource_works_controller_behavior.rb
@@ -7,20 +7,9 @@ module ScholarsArchive
     extend ActiveSupport::Concern
     include Hyrax::WorksControllerBehavior
 
-    after_action :email_for_accessibility_attestation, only: %i[create]
-
     def new
       curation_concern.rights_statement = ['http://rightsstatements.org/vocab/InC/1.0/']
       super
-    end
-
-    private
-
-    def email_for_accessibility_attestation
-      return unless params[hash_key_for_curation_concern]['attest'] == 'false'
-
-      ScholarsArchive::AttestationMailer.accessibility_attestation_mail(current_user.email, curation_concern).deliver_now
-      ScholarsArchive::UserAttestationMailer.user_attestation_mail(current_user.email).deliver_now
     end
   end
 end
